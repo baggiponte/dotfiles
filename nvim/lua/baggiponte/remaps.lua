6106cf4f735d8351c6897e3002258e4db1e9b110
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-09-13 18:54:37 +0200

    feat(remaps): add vsplit remap

--- nvim/lua/baggiponte/remaps.lua
+++ nvim/lua/baggiponte/remaps.lua
@@ -8,11 +8,13 @@ vim.keymap.set('n', '<c-j>', '<c-w>j', opts)
 vim.keymap.set('n', '<c-k>', '<c-w>k', opts)
 vim.keymap.set('n', '<c-l>', '<c-w>l', opts)
 
+-- [[ Create splits ]]
+vim.keymap.set('n', '<leader>s', '<cmd>vsp .<CR>', opts)
+
 -- [[ Save and exit files ]]
 vim.keymap.set('n', '<leader>w', '<cmd>w<CR>', opts) -- save file
 vim.keymap.set('n', '<leader>q', '<cmd>bd<CR>', opts) -- quit buffer
 vim.keymap.set('n', '<leader>Q', '<cmd>q<CR>', opts) -- quit all buffers
-vim.keymap.set('n', '<leader>pv', '<cmd>Ex<CR>', opts) -- launch netrw
 
 -- [[ General remaps ]]
 vim.keymap.set('', 'H', '^') -- remap go to the beginning of the line
