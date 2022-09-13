ee21c996bd10ba3cb41ecf720e1a44eb6396f086
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-09-13 18:27:36 +0200

    feat(telescope): add new file browser shortcut
    <leader>fD opens the file browser in the same directory as the current buffer

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -17,7 +17,7 @@ telescope.setup({
     buffers = {
       mappings = {
         i = {
-          ['<c-d>'] = 'delete_buffer',
+          ['<C-d>'] = 'delete_buffer',
         },
         n = {
           ['d'] = 'delete_buffer',
@@ -47,12 +47,18 @@ vim.keymap.set('n', '<leader>fH', require('telescope.builtin').help_tags, opts)
 vim.keymap.set('n', '<leader>fh', require('telescope.builtin').treesitter, opts)
 
 -- explore files
-vim.keymap.set('n', '<leader>fF', require('telescope.builtin').find_files, opts)
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').git_files, opts)
+vim.keymap.set('n', '<leader>fF', require('telescope.builtin').find_files, opts)
 vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, opts)
 
 -- fuzzy find and file browser
 vim.keymap.set('n', '<leader>fz', require('telescope.builtin').current_buffer_fuzzy_find, opts)
 vim.keymap.set('n', '<leader>fZ', require('telescope.builtin').live_grep, opts)
 vim.keymap.set('n', '<leader>fd', telescope.extensions.file_browser.file_browser, opts)
-vim.keymap.set('n', '<leader>fD', telescope.extensions.frecency.frecency, opts)
+vim.keymap.set(
+  'n',
+  '<leader>fD',
+  "<cmd>lua require 'telescope'.extensions.file_browser.file_browser{ path = '%:p:h' }<CR>",
+  opts
+) -- open in current buffer directory
+vim.keymap.set('n', '<leader>fr', telescope.extensions.frecency.frecency, opts)
