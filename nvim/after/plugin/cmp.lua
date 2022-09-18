73c2d7300ebab9332f5194195531ee3cf78aa65b
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-09-18 21:06:10 +0200

    feat(nvim): change cmp keymapping
    now <esc> exits completion menu (much better than <c-e>)

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -25,7 +25,7 @@ cmp.setup({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-n>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
+    ['<esc>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
