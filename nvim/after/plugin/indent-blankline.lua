07e2dff607a9c4a711f35c020437eb562d726bd3
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-09-18 18:43:10 +0200

    feat(nvim): update indent configs

--- nvim/after/plugin/indent-blankline.lua
+++ nvim/after/plugin/indent-blankline.lua
@@ -2,5 +2,8 @@ require('indent_blankline').setup({
   -- for example, context is off by default, use this to turn it on
   char = '┊',
   show_current_context = true,
-  show_current_context_start = true,
+  show_current_context_start = false,
+  use_treesitter = true,
+  buftype_exclude = { 'terminal' },
+  filetype_exclude = { 'help', 'packer', 'TelescopePrompt' },
 })
