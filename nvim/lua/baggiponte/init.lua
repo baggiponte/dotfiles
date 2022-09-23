acc83dbb6d6662a9678547c2c69fae38fa676ee9
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-09-23 10:22:40 +0200

    refactor(nvim): put packer on top so it bootstrap before calling any other plugin call

--- nvim/lua/baggiponte/init.lua
+++ nvim/lua/baggiponte/init.lua
@@ -1,6 +1,7 @@
+require('baggiponte.packer') -- load plugins
+
 require('impatient') -- impatient plugin to speedup startup
 require('impatient').enable_profile() -- enable profiling with impatient
 
 require('baggiponte.settings') -- tweaking configurations (e.g. incremental search)
 require('baggiponte.remaps') -- keyboard remappings
-require('baggiponte.packer') -- load plugins
