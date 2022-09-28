83a4c7e0c0437c8184bfd482e6d62ad70906649c
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-09-28 14:40:50 +0200

    fix(nvim): remove duplicate plugin settings file

--- nvim/after/plugin/symbols-outline.lua
+++
@@ -1,8 +0,0 @@
-local symbols_outline = require('symbols-outline')
-
--- setup
-symbols_outline.setup({})
-
--- keymaps
-local opts = { silent = true, noremap = true }
-vim.keymap.set('n', '<leader>fs', symbols_outline.toggle_outline, opts)
