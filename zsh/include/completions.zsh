f7706cd8d83b006f4cc096192d9b4d6ae413c834
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-09-28 15:07:22 +0200

    refactor(zsh): move zstyle and completions in dedicated module

--- zsh/include/completions.zsh
+++ zsh/include/completions.zsh
@@ -1,39 +1,19 @@
-# +-----------------+
-# | COMPLETION MENU |
-# +-----------------+
+# +------------------+
+# | PLUGINS WITH ZIM |
+# +------------------+
 
-zmodload zsh/complist
+# download zimfw plugin manager if missing
+if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+    https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+fi
 
-# use vim keys to navigate the menu selection
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'j' vi-down-line-or-history
+# install missing modules, and update ${zim_home}/init.zsh if missing or outdated
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
+  source ${ZIM_HOME}/zimfw.zsh init -q
+fi
 
-# type of completion, e.g. suggests correction if there's a typo
-zstyle ':completion:*' completer _extensions _complete _approximate
-
-# enable completion menu
-zstyle ':completion:*' menu select
-
-# syntax highlight for file completion
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-
-# use cache for tab completion
-zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/zcompcache"
-# use only if you use zmodule completion in your .zimrc: https://github.com/zimfw/completion#settings
-# zstyle ':zim:completion' dumpfile "$XDG_CACHE_HOME/zsh/zcompdump"
-
-# descriptors and corrections
-zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
-zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
-zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
-zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
-zstyle ':completion:*' group-name ''
-
-# case insensitivity and tab expansion
-zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+source ${ZIM_HOME}/init.zsh
 
 # +-------------------+
 # | OTHER COMPLETIONS |
@@ -46,3 +26,24 @@ fpath=($XDG_CACHE_HOME/zsh/zfunc $fpath)
 if [ -s $XDG_CACHE_HOME/zsh/zfunc/_pdm ]; then
   pdm completion zsh > $XDG_CACHE_HOME/zsh/zfunc/_pdm
 fi
+
+if [ -s $XDG_CACHE_HOME/zsh/zfunc/_arduino-cli ]; then
+  arduino-cli completion zsh > $XDG_CACHE_HOME/zsh/zfunc/_arduino-cli
+fi
+
+# +------------------------+
+# | INITIALISE COMPLETIONS |
+# +------------------------+
+
+# compinit is to be run last, after loading modules with zmodload etc
+# https://unix.stackexchange.com/a/391670/402599
+autoload -Uz compinit && compinit -d $XDG_CACHE_HOME/zsh/zcompdump # comment if using zim's completion zmodule
+autoload -Uz bashcompinit && bashcompinit
+
+# `eval` commands are run **after** compinit
+command -v jump >/dev/null && eval "$(jump shell zsh)"                               # autojump between directories; it's jump, not autojump!
+command -v pyenv >/dev/null && eval "$(pyenv init --path)" && eval "$(pyenv init -)" # python version manager
+command -v pyenv-virtualenv >/dev/null && eval "$(pyenv virtualenv-init -)"          # python virtualenv manager
+command -v python >/dev/null && eval "$(python -m pip completion --zsh)"             # autocompletion for pip
+command -v pipx >/dev/null && eval "$(register-python-argcomplete pipx)"             # autocompletion for pipx
+command -v direnv >/dev/null && eval "$(direnv hook zsh)"                            # load project specific env variables
---
+++ zsh/include/zstyle.zsh
@@ -0,0 +1,36 @@
+# +-----------------+
+# | COMPLETION MENU |
+# +-----------------+
+
+zmodload zsh/complist
+
+# use vim keys to navigate the menu selection
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+
+# type of completion, e.g. suggests correction if there's a typo
+zstyle ':completion:*' completer _extensions _complete _approximate
+
+# enable completion menu
+zstyle ':completion:*' menu select
+
+# syntax highlight for file completion
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+# use cache for tab completion
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/zcompcache"
+# use only if you use zmodule completion in your .zimrc: https://github.com/zimfw/completion#settings
+# zstyle ':zim:completion' dumpfile "$XDG_CACHE_HOME/zsh/zcompdump"
+
+# descriptors and corrections
+zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*' group-name ''
+
+# case insensitivity and tab expansion
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
