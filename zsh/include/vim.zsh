16d4f0b7af2950256589416d87a126cad41638e0
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-09-28 15:06:39 +0200

    refactor(zsh): move cursor function into include/vim.zsh

--- zsh/include/vim.zsh
+++ zsh/include/vim.zsh
@@ -7,7 +7,45 @@ bindkey -v
 export KEYTIMEOUT=1
 
 # change cursor
-source "$ZDOTDIR/include/cursor.zsh"
+cursor_mode() {
+    # See https://ttssh2.osdn.jp/manual/4/en/usage/tips/vim.html for cursors
+    cursor_block='\e[2 q'
+    cursor_beam='\e[6 q'
+
+    function zle-keymap-select {
+        if [[ ${KEYMAP} == vicmd ]] ||
+            [[ $1 = 'block' ]]; then
+            echo -ne $cursor_block
+        elif [[ ${KEYMAP} == main ]] ||
+            [[ ${KEYMAP} == viins ]] ||
+            [[ ${KEYMAP} = '' ]] ||
+            [[ $1 = 'beam' ]]; then
+            echo -ne $cursor_beam
+        fi
+    }
+
+    zle-line-init() {
+        echo -ne $cursor_beam
+    }
+
+    # If you have a problem with End and Home key
+    #    zle-line-init () {
+    #       # Default zle-line-init
+    #       if (( $+terminfo[smkx] ))
+    #       then
+    #               echoti smkx
+    #       fi
+    #       zle editor-info
+    #
+    #       # Modify cursor!
+    #       zle -K viins
+    #   }
+
+    zle -N zle-keymap-select
+    zle -N zle-line-init
+}
+
+cursor_mode
 
 # add vi text-objects for brackets and quotes, i.e. can be used with `a` (around) and `i` (inside)
 autoload -Uz select-bracketed select-quoted
