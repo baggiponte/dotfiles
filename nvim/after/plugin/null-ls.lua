6f8a88cfd46689e4061b20763300c6e1f9c303bc
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-09-29 15:09:00 +0200

    refactor(nvim): run null-ls shell formatters with sh bash and zsh filetypes

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -6,18 +6,17 @@ null_ls.setup({
     null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.diagnostics.luacheck,
     -- null_ls.builtins.diagnostics.mypy, -- not using
-    null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.diagnostics.shellcheck.with({ filetypes = { 'sh', 'bash', 'zsh' } }),
     null_ls.builtins.diagnostics.yamllint,
     -- formatters
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.jq,
     null_ls.builtins.formatting.isort,
     null_ls.builtins.formatting.markdownlint,
-    -- null_ls.builtins.formatting.markdowntoc, -- not available with Mason
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.yamlfmt, -- needs go
-    null_ls.builtins.formatting.shfmt, -- needs go
-    null_ls.builtins.formatting.shellharden, -- needs rust
+    null_ls.builtins.formatting.shfmt.with({ filetypes = { 'sh', 'bash', 'zsh' } }), -- needs go
+    null_ls.builtins.formatting.shellharden.with({ filetypes = { 'sh', 'bash', 'zsh' } }), -- needs rust
     -- hover actions
     null_ls.builtins.hover.printenv,
   },
