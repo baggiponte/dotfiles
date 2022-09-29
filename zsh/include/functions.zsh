69b8373c2ba8e896657e1be405270764b9e4f38d
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-09-29 15:43:21 +0200

    fix(zsh): adopt shellcheck recommendations

--- zsh/include/functions.zsh
+++ zsh/include/functions.zsh
@@ -6,35 +6,35 @@
 trash () { command mv "$@" ~/.Trash ; }
 
 # make a directory and cd into it
-take () { command mkdir -p "$1" && command cd "$1"; }
+take () { command mkdir -p "$1" && cd "$1"; }
 
 # create a custom function to use gitignore.io to create .gitignore files
-gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/"$@" ;}
+gi() { curl -sLw "\n" "https://www.toptal.com/developers/gitignore/api/$1" ;}
 
 # extract files
 extract() {
  ex() {
     case $1 in
-        *.tar.bz2)  tar xjf $1      ;;
-        *.tar.gz)   tar xzf $1      ;;
-        *.bz2)      bunzip2 $1      ;;
-        *.gz)       gunzip $1       ;;
-        *.tar)      tar xf $1       ;;
-        *.tbz2)     tar xjf $1      ;;
-        *.tgz)      tar xzf $1      ;;
-        *.zip)      unzip $1        ;;
-        *.7z)       7z x $1         ;; # require p7zip
-        *.rar)      7z x $1         ;; # require p7zip
-        *.iso)      7z x $1         ;; # require p7zip
-        *.Z)        uncompress $1   ;;
-        *)          echo "'$1' cannot be extracted" ;;
+        *.tar.bz2)  tar xjf "$1"      ;;
+        *.tar.gz)   tar xzf "$1"      ;;
+        *.bz2)      bunzip2 "$1"      ;;
+        *.gz)       gunzip "$1"       ;;
+        *.tar)      tar xf "$1"       ;;
+        *.tbz2)     tar xjf "$1"      ;;
+        *.tgz)      tar xzf "$1"      ;;
+        *.zip)      unzip "$1"        ;;
+        *.7z)       7z x "$1"         ;; # require p7zip
+        *.rar)      7z x "$1"         ;; # require p7zip
+        *.iso)      7z x "$1"         ;; # require p7zip
+        *.Z)        uncompress "$1"   ;;
+        *)          echo "$1 cannot be extracted" ;;
     esac
     }
 
     for file in "$@"
     do
-        if [ -f $file ]; then
-            ex $file
+        if [ -f "$file" ]; then
+            ex "$file"
         else
             echo "'$file' is not a valid file"
         fi
@@ -63,7 +63,7 @@ if [ "$(sysctl -n kern.ostype)" = "Darwin" ]; then
         if [ "$1" = "" ]; then
             open -a Finder ./
         else
-            open -a Finder $1
+            open -a Finder "$1"
         fi
     }
 fi
@@ -90,19 +90,19 @@ if hash brew 2>/dev/null; then
 
         # get the list of all files
         local download_dir="$HOME/Library/Caches/Homebrew/downloads"
-        local files=($download_dir/*(N))
+        local files=("$download_dir"/*(N))
 
         # if the number of files is not 0, then remove them
         # see: https://unix.stackexchange.com/a/313187/402599
         if (($#files)); then
-            echo "Removing downloads in $download_dir" && rm -- $files
+            echo "Removing downloads in $download_dir" && rm -- "${files[@]}"
         else
             echo "No downloads to remove"
         fi
 
         echo "Dumping formulae and casks to Brewfile..."
 
-        # see the comment here: https://unix.stackexchange.com/a/313187/402599
+        # using `:h` zsh modifier (run `info zsh modifiers`)
         pushd "$HOMEBREW_BUNDLE_FILE:h"
         mv Brewfile Brewfile.bak
         brew bundle dump --describe --no-upgrade
@@ -133,13 +133,13 @@ if hash nvim 2>/dev/null; then
         fi
     }
 
-    alias nrc="n $CONFIG/nvim"
-    alias nsh="n $ZDOTDIR"
+    alias nrc='n $CONFIG/nvim'
+    alias nsh='n $ZDOTDIR'
 
     nvim-update () {
         echo "updating nvim plugins..."
         nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-        echo "\ndone!"
+        echo "done!"
     }
 fi
 
