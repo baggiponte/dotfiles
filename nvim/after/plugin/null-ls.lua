66cce6892f4f260e140ef1ac1c2bc9edd4c769bc
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-10-06 10:30:38 +0200

    feat(nvim): use selene instead of luacheck as lua linter

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -2,7 +2,7 @@ local null_ls = require('null-ls')
 
 null_ls.setup({
   sources = {
-    -- linters
+    -- [[ python ]]
     null_ls.builtins.diagnostics.flake8.with({
       extra_args = {
         '--max-line-length=88',
@@ -12,20 +12,28 @@ null_ls.setup({
         "--per-file-ignores='__init__.py:F401'",
       },
     }),
-    null_ls.builtins.diagnostics.luacheck,
-    -- null_ls.builtins.diagnostics.mypy, -- not using
-    null_ls.builtins.diagnostics.shellcheck.with({ filetypes = { 'sh', 'bash', 'zsh' } }),
-    null_ls.builtins.diagnostics.yamllint,
-    -- formatters
+    -- null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.jq,
-    null_ls.builtins.formatting.isort.with({ extra_args = { '--profile=black', '--filter-files' } }),
-    null_ls.builtins.formatting.markdownlint,
+    null_ls.builtins.formatting.isort.with({
+      extra_args = { '--profile=black', '--filter-files' },
+    }),
+    -- [[ lua ]]
+    null_ls.builtins.diagnostics.selene.with({
+      extra_args = { '--config=' .. vim.fn.expand('$XDG_CONFIG_HOME/selene.toml') },
+    }),
     null_ls.builtins.formatting.stylua,
+    -- [[ yaml ]]
+    null_ls.builtins.diagnostics.yamllint,
     null_ls.builtins.formatting.yamlfmt, -- requires go
+    -- [[ json ]]
+    null_ls.builtins.formatting.jq,
+    -- [[ shell ]]
+    null_ls.builtins.diagnostics.shellcheck.with({ filetypes = { 'sh', 'bash', 'zsh' } }),
     null_ls.builtins.formatting.shfmt.with({ filetypes = { 'sh', 'bash', 'zsh' } }), -- requires go
     null_ls.builtins.formatting.shellharden.with({ filetypes = { 'sh', 'bash', 'zsh' } }), -- requires rust
-    -- hover actions
+    -- [[ markdown ]]
+    -- null_ls.builtins.formatting.markdownlint,
+    -- [[ other ]]
     null_ls.builtins.hover.printenv,
   },
 })
