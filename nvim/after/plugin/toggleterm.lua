511a384507d3bc5b37b2291340717050a1c45edb
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-10-06 19:28:49 +0200

    refactor(nvim): update toggleterm keybindings

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -7,5 +7,4 @@ require('toggleterm').setup({
 
 local opts = { silent = true, noremap = true }
 vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm<CR>', opts)
-vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-vim.keymap.set('t', '<C-esc>', [[<C-\><C-n><cmd>q<CR>]], opts)
+vim.keymap.set('t', '<esc>', [[<C-\><C-n><cmd>q<CR>]], opts)
