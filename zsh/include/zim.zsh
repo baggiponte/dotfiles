cd6b929b3c09a356925adbd2c56b7149dec189f1
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-10-16 22:41:07 +0200

    style(dotfiles): run formatters

--- zsh/include/zim.zsh
+++ zsh/include/zim.zsh
@@ -6,13 +6,13 @@ export ZIM_HOME="$XDG_CACHE_HOME/zim"
 
 # download zimfw plugin manager if missing
 if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
-  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+  curl -fsSL --create-dirs -o "${ZIM_HOME}/zimfw.zsh" \
     https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
 fi
 
 # install missing modules, and update ${zim_home}/init.zsh if missing or outdated
 if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
-  source ${ZIM_HOME}/zimfw.zsh init -q
+  source "${ZIM_HOME}/zimfw.zsh" init -q
 fi
 
-source ${ZIM_HOME}/init.zsh
+source "${ZIM_HOME}/init.zsh"
