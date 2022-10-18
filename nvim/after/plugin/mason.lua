3237960b72b5e68fd30f5e3496abc41bcf2e226d
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-10-18 11:27:55 +0200

    fix(nvim): use lsp only in mason ensure_installed

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -1,4 +1,4 @@
-local servers = {
+local sources = {
   'bashls',
   'dockerls',
   'jsonls',
@@ -8,37 +8,6 @@ local servers = {
   'sumneko_lua',
   'yamlls',
 }
-
-local linters = {
-  'flake8',
-  'selene',
-  'shellcheck',
-  'sqlfluff',
-  'yamllint',
-}
-
-local formatters = {
-  'black',
-  'isort',
-  'jq',
-  'shfmt',
-  'stylua',
-  'yamlfmt',
-}
-
-local sources = {}
-
-for _, server in ipairs(servers) do
-  table.insert(sources, server)
-end
-
-for _, linter in ipairs(linters) do
-  table.insert(sources, linter)
-end
-
-for _, formatter in ipairs(formatters) do
-  table.insert(sources, formatter)
-end
 
 require('mason').setup({})
 
