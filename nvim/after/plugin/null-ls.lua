dbdbebc6f8527dedcc18e1af29018f8b35eec29a
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-10-25 11:41:57 +0200

    feat(nvim): add arduino and cpp lang servers and formatters

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -4,11 +4,14 @@ local formatting = require('null-ls').builtins.formatting
 
 null_ls.setup({
   sources = {
+    diagnostics.cpplint.with({ filetypes = { 'arduino', 'c', 'cpp', 'cs', 'cuda' } }),
     diagnostics.flake8.with({ extra_args = { '--config=' .. vim.fn.expand('$XDG_CONFIG_HOME/.flake8') } }),
     diagnostics.selene.with({ extra_args = { '--config=' .. vim.fn.expand('$XDG_CONFIG_HOME/selene.toml') } }),
     diagnostics.shellcheck.with({ filetypes = { 'sh', 'bash', 'zsh' } }),
     diagnostics.yamllint,
     diagnostics.actionlint,
+    -- is it builtin clangd server?
+    -- formatting.clang_format.with({ filetypes = { 'arduino', 'c', 'cpp', 'cs', 'cuda' } }),
     formatting.black,
     formatting.isort.with({ extra_args = { '--profile=black', '--filter-files' } }),
     formatting.jq,
