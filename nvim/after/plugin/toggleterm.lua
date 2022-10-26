86259173501e8009eb665e879b2fee9057096bf0
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-10-26 18:12:04 +0200

    feat(nvim): replace lazygit.nvim with toggleterm command

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -1,3 +1,10 @@
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = 'lazygit', hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
 require('toggleterm').setup({
   direction = 'float',
   float_opts = {
@@ -7,4 +14,6 @@ require('toggleterm').setup({
 
 local opts = { silent = true, noremap = true }
 vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm<CR>', opts)
-vim.keymap.set('t', '<esc>', [[<C-\><C-n><cmd>q<CR>]], opts)
+vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n><cmd>q<CR>]], opts)
+
+vim.api.nvim_set_keymap('n', 'lg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
