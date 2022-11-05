b54b5155a9bb5814195b51ac5790e84154b2563d
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-11-05 21:55:07 +0100

    refactor(nvim): replace strings with vim.cmd calls in toggleterm keymaps
    refactor(nvim): rewrite global keymaps using functions and vim.cmd instead of strings

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -1,9 +1,5 @@
 local Terminal = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = 'lazygit', hidden = true, direction = 'float' })
-
-function _lazygit_toggle()
-  lazygit:toggle()
-end
+local lazygit = Terminal:new({ cmd = 'lazygit', hidden = true, direction = 'float' }) -- see: https://github.com/akinsho/toggleterm.nvim#custom-terminals
 
 require('toggleterm').setup({
   direction = 'float',
@@ -13,7 +9,11 @@ require('toggleterm').setup({
 })
 
 local opts = { silent = true, noremap = true }
-vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm<CR>', opts)
+vim.keymap.set('n', '<leader>tt', function()
+  vim.cmd('ToggleTerm ' .. 'dir=' .. vim.fn.expand('%:p:h'))
+end, opts)
 vim.keymap.set('t', '<esc><esc>', [[<C-\><C-n><cmd>q<CR>]], opts)
 
-vim.api.nvim_set_keymap('n', '<leader>lg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>lg', function()
+  lazygit:toggle()
+end, { noremap = true, silent = true })
