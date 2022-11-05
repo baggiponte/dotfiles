01db1022387605e7f312185e6a92ac4c1596e269
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-11-05 19:43:02 +0100

    feat(zsh): update history options

--- zsh/include/options.zsh
+++ zsh/include/options.zsh
@@ -23,11 +23,11 @@ setopt rm_star_silent           # rm ./* does not request confirmation
 # history
 setopt extended_history         # write the history file in the ':start:elapsed;command' format.
 setopt share_history            # share history between all sessions.
-setopt hist_expire_dups_first   # expire a duplicate event first when trimming history.
+# setopt hist_expire_dups_first   # expire a duplicate event first when trimming history.
 setopt hist_ignore_dups         # do not record an event that was just recorded again.
-setopt hist_ignore_all_dups     # delete an old recorded event if a new event is a duplicate.
-setopt hist_find_no_dups        # do not display a previously found event.
+# setopt hist_ignore_all_dups     # delete an old recorded event if a new event is a duplicate.
+# setopt hist_find_no_dups        # do not display a previously found event.
 setopt hist_ignore_space        # do not record an event starting with a space.
-setopt hist_save_no_dups        # do not write a duplicate event to the history file.
+# setopt hist_save_no_dups        # do not write a duplicate event to the history file.
 setopt hist_verify              # do not execute immediately upon history expansion.
 setopt hist_reduce_blanks       # remove blank lines
