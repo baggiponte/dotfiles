5c0f0f25bc8250b8c75676529c124670ddc68932
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-11-10 23:25:29 +0100

    feat(pyenv): re-add pyenv-virtualenv to work with futura
    feat(brew): re-add pyenv-virtualenv to brewfile

--- zsh/include/completions.zsh
+++ zsh/include/completions.zsh
@@ -26,6 +26,7 @@ autoload -Uz bashcompinit && bashcompinit
 # `eval` commands are run **after** compinit
 command -v zoxide >/dev/null && eval "$(zoxide init zsh)"                               # autojump between directories; it's jump, not autojump!
 command -v pyenv >/dev/null && eval "$(pyenv init -)" # python version manager
+command -v pyenv-virtualenv-init >/dev/null && eval "$(pyenv virtualenv-init -)"
 command -v python >/dev/null && eval "$(python -m pip completion --zsh)"             # autocompletion for pip
 command -v pipx >/dev/null && eval "$(register-python-argcomplete pipx)"             # autocompletion for pipx
 command -v direnv >/dev/null && eval "$(direnv hook zsh)"                            # load project specific env variables
