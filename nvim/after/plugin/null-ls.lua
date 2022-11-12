1f478c236590e44b5ff20995e28a745071952d62
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-11-13 00:43:21 +0100

    feat(nvim): auto format on save with null-ls

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -2,6 +2,8 @@ local null_ls = require('null-ls')
 local diagnostics = require('null-ls').builtins.diagnostics
 local formatting = require('null-ls').builtins.formatting
 
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
 null_ls.setup({
   sources = {
     diagnostics.cpplint.with({ filetypes = { 'arduino', 'c', 'cpp', 'cs', 'cuda' } }),
@@ -20,4 +22,16 @@ null_ls.setup({
     formatting.stylua,
     formatting.yamlfmt, -- only one that cannot be installed with brew, requires go + mason
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method('textDocument/formatting') then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
 })
