396f4c31aa9a93d35ad34539bc5daa152b83ab50
baggiponte <57922983+baggiponte@users.noreply.github.com>
2022-12-14 15:44:06 +0100

    fix(nvim): [TROUBLE] replace keymaps

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -6,8 +6,8 @@ trouble.setup({
 
 local opts = { silent = true, noremap = true }
 
-vim.keymap.set('n', '<leader>dd', '<cmd>TroubleToggle<cr>', opts) -- <leader>dD is telescope diagnostics
+vim.keymap.set('n', '<leader>dD', '<cmd>TroubleToggle<cr>', opts) -- <leader>dD is telescope diagnostics
 vim.keymap.set('n', '<leader>dw', '<cmd>TroubleToggle workspace_diagnostics<cr>', opts)
-vim.keymap.set('n', '<leader>dD', '<cmd>TroubleToggle document_diagnostics<cr>', opts)
+vim.keymap.set('n', '<leader>dd', '<cmd>TroubleToggle document_diagnostics<cr>', opts)
 vim.keymap.set('n', '<leader>dl', '<cmd>TroubleToggle loclist<cr>', opts)
 vim.keymap.set('n', '<leader>dq', '<cmd>TroubleToggle quickfix<cr>', opts)
