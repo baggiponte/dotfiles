0ac8833939ae3bf289f3eda86c1793162c4c5dbe
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-01-28 17:35:13 +0100

    feat(nvim): move autocmds to dedicated module

---
+++ nvim/lua/baggiponte/autocmds.lua
@@ -0,0 +1,19 @@
+-- [[ Highlight on yank ]]
+vim.api.nvim_exec(
+  [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]],
+  false
+)
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'TelescopePrompt' },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set('n', 'q', '<cmd>close!<cr>', { buffer = event.buf, silent = true })
+  end,
+})
