02d8cb8e951c74c5d40f3752f07864ebf83cf666
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-01-29 18:47:07 +0100

    fix(nvim): [sayonara] config settings does not apply, move to dedicated module

---
+++ nvim/after/plugin/sayonara.lua
@@ -0,0 +1,7 @@
+vim.keymap.set('n', 'q', [[<cmd>Sayonara<cr>]], {
+  desc = '[q]uit current buffer with Sayonara',
+})
+
+vim.keymap.set('n', 'Q', [[<cmd>Sayonara!<cr>]], {
+  desc = '[Q]uit current buffer with Sayonara (force)',
+})
