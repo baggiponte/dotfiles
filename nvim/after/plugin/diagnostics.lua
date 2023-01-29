f3c77c4bebe7b7a335e50b781f86f98c3dceeb0c
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-01-29 18:00:03 +0100

    feat(nvim): [packer] update after/plugin

--- nvim/after/plugin/diagnostics.lua
+++
@@ -1,6 +0,0 @@
--- see https://smarttech101.com/nvim-lsp-diagnostics-keybindings-signs-virtual-texts/
-local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
