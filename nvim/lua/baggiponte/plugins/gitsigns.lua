ce1cf08cb0b093ace709ddc3219ad12c1458bcb4
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-01-29 19:40:18 +0100

    fix(nvim): [lazy] fix first batch of errors

--- nvim/lua/baggiponte/plugins/gitsigns.lua
+++ nvim/lua/baggiponte/plugins/gitsigns.lua
@@ -1,9 +1,12 @@
-require('gitsigns').setup({
-  signs = {
-    add = { hl = 'GitGutterAdd', text = '|' },
-    change = { hl = 'GitGutterChange', text = '|' },
-    delete = { hl = 'GitGutterDelete', text = '_' },
-    topdelete = { hl = 'GitGutterDelete', text = '‾' },
-    changedelete = { hl = 'GitGutterChange', text = '~' },
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    signs = {
+      add = { hl = 'GitGutterAdd', text = '|' },
+      change = { hl = 'GitGutterChange', text = '|' },
+      delete = { hl = 'GitGutterDelete', text = '_' },
+      topdelete = { hl = 'GitGutterDelete', text = '‾' },
+      changedelete = { hl = 'GitGutterChange', text = '~' },
+    },
   },
-})
+}
