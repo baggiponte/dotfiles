f649200e5be709e3db9d5a782a3634891dada2c2
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-01-29 18:20:29 +0100

    feat(nvim): [lazy]
    try new setup

---
+++ nvim/lua/baggiponte/plugins/dap.lua
@@ -0,0 +1,46 @@
+return {
+  { 'rcarriga/nvim-dap-ui', name = 'dapui', config = true },
+  {
+    'mfussenegger/nvim-dap-python',
+    config = function()
+      local dappy = require('dap-python')
+      dappy.setup(vim.fn.stdpath('data') .. '/mason/packages/debugpy/venv/bin/python')
+      dappy.test_runner = 'pytest'
+    end,
+  },
+  {
+    'mfussenegger/nvim-dap',
+    keys = {
+      {
+        '<F5>',
+        require('dap').continue,
+        desc = 'Start debugger session',
+        silent = true,
+        noremap = true,
+      },
+      {
+        '<leader>b',
+        require('dap').toggle_breakpoint,
+        desc = 'Toggle debugger [b]reakpoint',
+        silent = true,
+        noremap = true,
+      },
+      config = function()
+        local dap = require('dap')
+        local dapui = require('dapui')
+
+        dap.listeners.after.event_initialized['dapui_config'] = function()
+          dapui.open({})
+        end
+
+        dap.listeners.before.event_terminated['dapui_config'] = function()
+          dapui.close({})
+        end
+
+        dap.listeners.before.event_exited['dapui_config'] = function()
+          dapui.close({})
+        end
+      end,
+    },
+  },
+}
