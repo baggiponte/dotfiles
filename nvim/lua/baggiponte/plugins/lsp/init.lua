80d397e968e66c8cba51b0428455265c9aab9f59
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-03-01 19:33:49 +0100

    refactor(nvim): [lsp] split configs in multiple files

---
+++ nvim/lua/baggiponte/plugins/lsp/init.lua
@@ -0,0 +1,88 @@
+local borders = require('baggiponte.utils.borders')
+local icons = require('baggiponte.utils.icons').icons
+local mason = require('baggiponte.plugins.lsp.sources').mason
+
+return {
+  {
+    { 'williamboman/mason.nvim', cmd = 'Mason', opts = { ui = { border = borders } } },
+    { 'jayp0521/mason-nvim-dap.nvim', cmd = 'Mason', opts = { ensure_installed = mason.dap } },
+    { 'jayp0521/mason-null-ls.nvim', cmd = 'Mason', opts = { ensure_installed = mason.null_ls } },
+    { 'smjonas/inc-rename.nvim', cmd = 'IncRename', config = true },
+    {
+      'glepnir/lspsaga.nvim',
+      event = 'VeryLazy',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      opts = {
+        lightbulb = { enable = false },
+        ui = { border = 'rounded', code_action = icons.diagnostics.Hint },
+      },
+    },
+    {
+      'jose-elias-alvarez/null-ls.nvim',
+      event = 'BufEnter',
+      dependencies = 'nvim-lua/plenary.nvim',
+      opts = function()
+        local diagnostics = require('null-ls').builtins.diagnostics
+        local formatting = require('null-ls').builtins.formatting
+
+        -- local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
+        return {
+          sources = {
+            -- diagnostics.semgrep,
+            -- diagnostics.shellharden,
+            -- diagnostics.vulture,
+            -- formatting.format_r.with({ filetypes = { 'r', 'rmd', 'quarto' } }),
+            -- formatting.styler.with({ filetypes = { 'r', 'rmd', 'quarto' } }),
+            diagnostics.actionlint,
+            diagnostics.mypy,
+            diagnostics.ruff,
+            diagnostics.selene.with({ extra_args = { '--config=' .. vim.fn.expand('$XDG_CONFIG_HOME/selene.toml') } }),
+            diagnostics.shellcheck.with({ filetypes = { 'sh', 'bash', 'zsh' } }),
+            diagnostics.yamllint.with({
+              extra_args = { '--config-data', '{extends: default, rules: {document-start: {present: false}}}' },
+            }),
+            formatting.black,
+            formatting.isort.with({ extra_args = { '--profile=black', '--filter-files' } }),
+            formatting.jq,
+            formatting.shfmt.with({ filetypes = { 'sh', 'bash', 'zsh' } }),
+            formatting.stylua,
+            formatting.yamlfmt, -- only one that cannot be installed with brew, requires go + mason
+          },
+          -- on_attach = function(client, bufnr)
+          --   if client.supports_method('textDocument/formatting') then
+          --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          --     vim.api.nvim_create_autocmd('BufWritePre', {
+          --       group = augroup,
+          --       buffer = bufnr,
+          --       callback = function()
+          --         vim.lsp.buf.format({ bufnr = bufnr })
+          --       end,
+          --     })
+          --   end
+          -- end,
+        }
+      end,
+    },
+    {
+      'neovim/nvim-lspconfig',
+      dependencies = { 'williamboman/mason-lspconfig.nvim', opts = { ensure_installed = mason.lsp } },
+      event = 'BufEnter',
+      config = function()
+        local servers = require('baggiponte.plugins.lsp.sources').servers
+
+        require('lspconfig.ui.windows').default_options.border = borders
+
+        for lsp, lsp_configs in pairs(servers) do
+          local configs = vim.tbl_deep_extend('force', {
+            capabilities = require('baggiponte.plugins.lsp.utils').capabilities,
+            on_attach = require('baggiponte.plugins.lsp.utils').on_attach,
+            handlers = require('baggiponte.plugins.lsp.utils').handlers,
+          }, lsp_configs)
+
+          require('lspconfig')[lsp].setup(configs)
+        end
+      end,
+    },
+  },
+}
---
+++ nvim/lua/baggiponte/plugins/lsp/sources.lua
@@ -0,0 +1,85 @@
+local M = {}
+
+M.mason = {
+  dap = {
+    'python',
+  },
+  lsp = {
+    'jsonls',
+    'lua_ls',
+    'pyright',
+    'ruff_lsp',
+    'sourcery',
+    -- 'arduino_language_server',
+    -- 'docker-compose-language-server',
+    -- 'dockerfile-language-server',
+    -- 'julials',
+    -- 'yamlls',
+  },
+  null_ls = {
+    'actionlint',
+    'black',
+    'isort',
+    'jq',
+    'ruff',
+    'selene',
+    'shellcheck',
+    'shfmt',
+    'stylua',
+    'yamlfmt',
+    'yamllint',
+    -- 'semgrep',
+    -- 'shellharden',
+    -- 'vulture',
+  },
+}
+
+M.servers = {
+  -- 'docker-compose-language-server',
+  -- 'dockerfile-language-server',
+  -- 'julials',
+  -- 'yamlls',
+  jsonls = {},
+  pyright = {},
+  ruff_lsp = {},
+  sourcery = {},
+  rust_analyzer = {
+    cmd = {
+      'rustup',
+      'run',
+      'stable',
+      'rust-analyzer',
+    },
+  },
+  lua_ls = {
+    settings = {
+      Lua = {
+        runtime = { version = 'LuaJIT' }, -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        diagnostics = { globals = { 'vim' } }, -- Get the language server to recognize the `vim` global
+        workspace = { checkThirdParty = false },
+        telemetry = { enable = false }, -- Do not send telemetry data containing a randomized but unique identifier
+      },
+    },
+  },
+  r_language_server = {
+    filetypes = { 'r', 'rmd', 'quarto' },
+  },
+  arduino_language_server = {
+    cmd = {
+      'arduino-language-server',
+      '-cli-config',
+      vim.fn.expand('$XDG_CONFIG_HOME') .. '/arduino/arduino-cli.yaml',
+      '-fqbn',
+      'arduino:avr:uno',
+      '-cli',
+      'arduino-cli',
+      '-clangd',
+      vim.fn.stdpath('data') .. '/' .. 'mason/bin/clangd',
+    },
+  },
+  clangd = {
+    filetypes = { 'arduino', 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+  },
+}
+
+return M
