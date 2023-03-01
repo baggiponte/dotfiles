d6c1ed41bfaa5fbe150c6bfa32f886472f0445a5
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-03-01 17:22:00 +0100

    feat(nvim): [colorscheme] add color in diagnostic virtual text

--- nvim/lua/baggiponte/plugins/colorscheme.lua
+++ nvim/lua/baggiponte/plugins/colorscheme.lua
@@ -4,6 +4,18 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      -- defaults
+      -- vim.g.gruvbox_material_background = 'medium'
+      -- vim.g.gruvbox_material_diagnostic_line_highlight = 1
+      -- vim.g.gruvbox_material_diagnostic_text_highlight = 0
+      -- vim.g.gruvbox_material_disable_italic_comment = 0
+      -- vim.g.gruvbox_material_enable_bold = 0
+      -- vim.g.gruvbox_material_foreground = 'material'
+      -- vim.g.gruvbox_material_ui_contrast = 'low'
+
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_diagnostic_virtual_text = 1
+      vim.g.gruvbox_material_enable_italic = 1
       vim.g.gruvbox_material_transparent_background = 1
       vim.cmd([[colorscheme gruvbox-material]])
 
