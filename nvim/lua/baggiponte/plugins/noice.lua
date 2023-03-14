0ce8d575975351281ba7dec9106fb9f84305dd2f
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-03-14 15:52:33 +0100

    refactor(nvim): [noice] update settings

--- nvim/lua/baggiponte/plugins/noice.lua
+++ nvim/lua/baggiponte/plugins/noice.lua
@@ -1,26 +1,24 @@
 return {
-  'folke/noice.nvim',
-  event = 'VeryLazy',
-  dependencies = { 'MunifTanjim/nui.nvim' },
-  config = function()
-    require('noice').setup({
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {
       cmdline = { enabled = true },
       messages = { enabled = false },
       lsp = {
+        progress = { enabled = false },
+        message = { enabled = true },
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
           ['vim.lsp.util.stylize_markdown'] = true,
           -- ['cmp.entry.get_documentation'] = true,
         },
-        hover = { enabled = true },
-        signature = { enabled = true },
-        message = { enabled = true },
-        documentation = { enabled = true },
       },
-      presets = {
-        -- inc_rename = true,
-        lsp_doc_border = true,
-      },
-    })
-  end,
+    },
+    presets = {
+      inc_rename = true,
+      lsp_doc_border = true,
+    },
+  },
 }
