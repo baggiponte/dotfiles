9d2ca3c4d4e76476b3cd63db40cfd33bd3bb6935
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-03-18 09:30:58 +0100

    feat(nvim): add nvim-tree keymap, fix conflict with neogen

--- nvim/lua/baggiponte/plugins/nvim-tree.lua
+++ nvim/lua/baggiponte/plugins/nvim-tree.lua
@@ -1,5 +1,12 @@
 return {
   'nvim-tree/nvim-tree.lua',
   cmd = 'NvimTreeToggle',
+  keys = {
+    {
+      '<Leader>N',
+      '<cmd>NvimTreeToggle<CR>',
+      mode = { 'n', 'v' },
+    },
+  },
   config = true,
 }
