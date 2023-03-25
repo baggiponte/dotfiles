1721222d9e6695585ce3c75b1da9a92423dbc474
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-03-25 17:38:48 +0100

    feat(zsh): add new zoxide zle keybinding

--- zsh/include/bindkeys.zsh
+++ zsh/include/bindkeys.zsh
@@ -10,3 +10,12 @@ bindkey '^o' live_grep
 
 zle -N fuzzy_find
 bindkey '^p' fuzzy_find
+
+# see: https://unix.stackexchange.com/a/703707/402599
+_zle_zoxide() {
+	zoxide-interactive
+	zle -I
+}
+
+zle -N _zle_zoxide
+bindkey '^j' _zle_zoxide
