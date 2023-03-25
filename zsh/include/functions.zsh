8358cea855bed89a04f0c5f02289d5486beb35e9
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-03-25 17:38:33 +0100

    refactor(zsh): update functions

--- zsh/include/functions.zsh
+++ zsh/include/functions.zsh
@@ -16,9 +16,9 @@ _requires() {
 _fuzzy_find() {
 	_requires fd fzf bat
 
-	local pattern="${1:-"."}"
+	local pattern="$1"
 
-	fd -t=f "$pattern" -HI \
+	fd -t=f -HI \
 		-E .DS_Store \
 		-E .git \
 		-E .mypy_cache \
@@ -26,26 +26,33 @@ _fuzzy_find() {
 		-E .venv \
 		-E __pycache__ \
 		-E assets \
-		-E raycast \
+		-E raycast |
 		fzf \
-		--preview="bat --color=always --style='plain,changes' --line-range=:500 {}"
+			--preview="bat --color=always --style='plain,changes' --line-range=:500 {}" \
+			--query="$pattern"
 }
 
 fuzzy_find() {
 	_requires nvim
 
-	local pattern="${1:-"."}"
+	local pattern="$1"
+	local file
+	file="$(_fuzzy_find "$pattern")"
 
-	nvim "$(_fuzzy_find "$pattern")"
+	if [ -z "$file" ]; then
+		return 1
+	else
+		nvim -- "$file"
+	fi
 }
 
 # grep string and pipe in fzf preview
 _live_grep() {
 	_requires rg fzf bat
 
-	local pattern="${1:-""}"
+	local pattern="$1"
 
-	rg "$pattern" \
+	rg \
 		--color=always \
 		--line-number \
 		--no-heading \
@@ -54,20 +61,21 @@ _live_grep() {
 			--ansi \
 			--color "hl:-1:underline,hl+:-1:underline:reverse" \
 			--delimiter : \
-			--preview 'bat --color=always {1} --highlight-line {2} --plain'
+			--preview 'bat --color=always {1} --highlight-line {2} --plain' \
+			--query "$pattern"
 }
 
 live_grep() {
 	_requires nvim
 
-	local pattern="${1:-""}"
+	local pattern="$1"
 	local file
 	file="$(_live_grep "$pattern" | cut -d : -f 1,2)"
 
 	if [ -z "$file" ]; then
 		return 1
 	else
-		nvim +"${file#*:}" "${file%:*}" -c "normal zz"
+		nvim +"${file#*:}" -c "normal zz" -- "${file%:*}"
 	fi
 }
 
@@ -84,49 +92,37 @@ n() {
 	elif [ -d "$1" ]; then
 		nvim -c "Telescope find_files cwd=$1"
 	else
-		nvim "$1"
+		nvim -- "$1"
 	fi
+}
+
+# open zoxide dir
+
+zoxide-interactive() {
+	_requires zoxide
+
+	local query="$1"
+	local chosen_directory
+
+	chosen_directory=$(
+		zoxide query \
+			--exclude "$PWD" \
+			--list |
+			fzf \
+				--preview="exa --all --group-directories-first --icons --oneline --ignore-glob={.DS_Store,.git} {}" \
+				--height 40% \
+				--preview-window=down,40% \
+				--preview-label=" content preview " \
+				--query="$query"
+	)
+
+	cd -- "$chosen_directory" || return 1
 }
 
 zoxide-clean() {
 	zoxide query --list |
 		fzf --multi --sort |
 		xargs -I % sh -c "rich --print 'deleting [blue]%[/]'; zoxide remove '%'"
-}
-
-# open zoxide dir
-
-_zoxide_with_preview() {
-	_requires nvim zoxide
-
-	local query="$1"
-	local chosen_directory
-
-	chosen_directory=$(
-		zoxide query --list |
-			fzf \
-				--preview="exa --all --group-directories-first --icons --oneline --ignore-glob={.DS_Store,.git} {}" \
-				--preview-window=down,40% \
-				--preview-label=" content preview " \
-				--query="$query"
-	)
-
-	cd "$chosen_directory" || return 1
-}
-
-jj() {
-	_zoxide_with_preview "$1"
-	fuzzy_find .
-}
-
-jk() {
-	_zoxide_with_preview "$1"
-	live_grep ""
-}
-
-jl() {
-	_zoxide_with_preview "$1"
-	lazygit
 }
 
 tn() {
