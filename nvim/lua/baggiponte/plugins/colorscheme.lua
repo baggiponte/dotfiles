e964f2184c64ccfeaa002fe71fc2acfcf23b6a6a
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-06-04 00:19:22 +0200

    refactor(nvim): moar transparency in colorscheme

--- nvim/lua/baggiponte/plugins/colorscheme.lua
+++ nvim/lua/baggiponte/plugins/colorscheme.lua
@@ -14,9 +14,9 @@ return {
       -- vim.g.gruvbox_material_ui_contrast = 'low'
 
       vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_diagnostic_virtual_text = 1
+      vim.g.gruvbox_material_diagnostic_virtual_text = 'highlighted'
       vim.g.gruvbox_material_enable_italic = 1
-      vim.g.gruvbox_material_transparent_background = 1
+      vim.g.gruvbox_material_transparent_background = 2
       vim.cmd([[colorscheme gruvbox-material]])
 
       for _, element in ipairs({ 'FloatBorder', 'NormalFloat', 'Normal' }) do
