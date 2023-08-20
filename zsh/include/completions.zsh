5b3cbad727de45bad2bcb789742d262c8ca6a6f4
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-08-20 22:17:10 +0200

    refactor(zsh): split autocompletions and evals

--- zsh/include/completions.zsh
+++ zsh/include/completions.zsh
@@ -32,19 +32,3 @@ fi
 # comment out if using zim's completion zmodule
 autoload -Uz compinit && compinit -d "${XDG_CACHE_HOME}/zsh/zcompdump"
 autoload -Uz bashcompinit && bashcompinit
-
-# `eval` commands are run **after** compinit
-# completions
-command -v cz >/dev/null && eval "$(register-python-argcomplete cz)"     # conventional commits + semver + keep a changelog
-command -v pipx >/dev/null && eval "$(register-python-argcomplete pipx)" # autocompletion for pipx
-command -v python >/dev/null && eval "$(python -m pip completion --zsh)" # autocompletion for pip
-command -v pixi >/dev/null && eval "$(pixi completion --shell zsh)"
-
-# +-------+
-# | HOOKS |
-# +-------+
-
-# hook binaries into zsh
-command -v direnv >/dev/null && eval "$(direnv hook zsh)"         # load project specific env variables
-command -v rtx >/dev/null && eval "$(rtx activate zsh)"           # hook rtx
-command -v zoxide >/dev/null && eval "$(zoxide init zsh --cmd=j)" # autojump between directories; it's jump, not autojump!
