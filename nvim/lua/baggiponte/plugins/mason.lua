180f423ae9a1a369d61e9a913f9418e204183d72
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-10-01 22:45:27 +0200

    refactor(nvim): split mason and nulls from lsp config

---
+++ nvim/lua/baggiponte/plugins/mason.lua
@@ -0,0 +1,29 @@
+local safe_require = require('baggiponte.utils').safe_require
+
+local borders = safe_require('baggiponte.utils.borders')
+local mason = safe_require('baggiponte.plugins.lsp.sources').mason
+
+return {
+  {
+    'williamboman/mason.nvim',
+    build = ':MasonUpdate',
+    cmd = { 'Mason', 'MasonUpdate' },
+    opts = {
+      -- PATH = 'append',
+      ui = {
+        border = borders,
+        icons = { package_installed = '✓', package_uninstalled = '✗', package_pending = '⟳' },
+      },
+    },
+  },
+  {
+    'jayp0521/mason-nvim-dap.nvim',
+    cmd = { 'Mason', 'DapInstall', 'DapUninstall' },
+    opts = { ensure_installed = mason.dap },
+  },
+  {
+    'jayp0521/mason-null-ls.nvim',
+    cmd = { 'Mason', 'NullLsInstall', 'NullLsUninstall' },
+    opts = { ensure_installed = mason.null_ls },
+  },
+}
