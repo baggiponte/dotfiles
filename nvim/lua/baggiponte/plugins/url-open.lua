d19caa914f2b5da8e40b727bf3d1857ae56344e5
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-10-01 21:07:38 +0200

    feat(nvim): [url-open] configure plugin

---
+++ nvim/lua/baggiponte/plugins/url-open.lua
@@ -0,0 +1,7 @@
+return {
+  'sontungexpt/url-open',
+  event = 'VeryLazy',
+  cmd = 'URLOpenUnderCursor',
+  keys = { 'gx', '<cmd>URLOpenUnderCursor<cr>', desc = 'Open the url under the cursor' },
+  opts = {},
+}
