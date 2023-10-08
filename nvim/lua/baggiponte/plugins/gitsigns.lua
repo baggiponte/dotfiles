dc7a74615b30a467f1e433815dbc4c2b34ac83b1
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-10-08 17:49:38 +0200

    refactor(nvim): split gitsigns and fugitive

--- nvim/lua/baggiponte/plugins/gitsigns.lua
+++ nvim/lua/baggiponte/plugins/gitsigns.lua
@@ -1,84 +1,66 @@
 local safe_require = require('baggiponte.utils').safe_require
 
+local keys = {
+    {
+      '<leader>hs',
+      '<cmd>Gitsigns stage_hunk<CR>',
+      mode = { 'n', 'v' },
+    },
+    {
+      '<leader>hr',
+      '<cmd>Gitsigns reset_hunk<CR>',
+      mode = { 'n', 'v' },
+    },
+    {
+      ']h',
+      function()
+        local gs = safe_require('gitsigns')
+
+        if vim.wo.diff then
+          return ']h'
+        end
+
+        vim.schedule(function()
+          gs.next_hunk()
+        end)
+
+        return '<Ignore>'
+      end,
+      expr = true,
+    },
+    {
+      '[h',
+      function()
+        local gs = safe_require('gitsigns')
+
+        if vim.wo.diff then
+          return '[h'
+        end
+
+        vim.schedule(function()
+          gs.prev_hunk()
+        end)
+
+        return '<Ignore>'
+      end,
+      expr = true,
+    },
+  }
+
 return {
-  {
-    'tpope/vim-fugitive',
-    cmd = 'Git',
-    keys = {
-      {
-        '<leader>G',
-        '<cmd>Git<CR>',
-        mode = { 'n', 'v' },
-        desc = 'Fugitive: toggle [g]it status window',
-      },
-      {
-        '<leader>gc',
-        '<cmd>Git commit<CR>',
-        mode = { 'n', 'v' },
-        desc = 'Fugitive: [g]it [c]ommit',
-      },
+  'lewis6991/gitsigns.nvim',
+  event = 'VeryLazy',
+  dependencies = 'nvim-lua/plenary.nvim',
+  keys = keys,
+  opts = {
+    signs = {
+      add = { hl = 'GitGutterAdd', text = '|' },
+      change = { hl = 'GitGutterChange', text = '|' },
+      delete = { hl = 'GitGutterDelete', text = '_' },
+      topdelete = { hl = 'GitGutterDelete', text = '‾' },
+      changedelete = { hl = 'GitGutterChange', text = '~' },
     },
-  },
-  {
-    'lewis6991/gitsigns.nvim',
-    event = 'VeryLazy',
-    dependencies = 'nvim-lua/plenary.nvim',
-    keys = {
-      {
-        '<leader>hs',
-        '<cmd>Gitsigns stage_hunk<CR>',
-        mode = { 'n', 'v' },
-      },
-      {
-        '<leader>hr',
-        '<cmd>Gitsigns reset_hunk<CR>',
-        mode = { 'n', 'v' },
-      },
-      {
-        ']h',
-        function()
-          local gs = safe_require('gitsigns')
-
-          if vim.wo.diff then
-            return ']h'
-          end
-
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-
-          return '<Ignore>'
-        end,
-        expr = true,
-      },
-      {
-        '[h',
-        function()
-          local gs = safe_require('gitsigns')
-
-          if vim.wo.diff then
-            return '[h'
-          end
-
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-
-          return '<Ignore>'
-        end,
-        expr = true,
-      },
-    },
-    opts = {
-      signs = {
-        add = { hl = 'GitGutterAdd', text = '|' },
-        change = { hl = 'GitGutterChange', text = '|' },
-        delete = { hl = 'GitGutterDelete', text = '_' },
-        topdelete = { hl = 'GitGutterDelete', text = '‾' },
-        changedelete = { hl = 'GitGutterChange', text = '~' },
-      },
-      current_line_blame = true,
-      preview_config = { border = 'round' },
-    },
-  },
+    current_line_blame = true,
+    preview_config = { border = 'round' },
+  }
 }
