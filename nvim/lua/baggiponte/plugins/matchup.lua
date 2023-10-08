37fa608f3d203f8f9bf3e7fb5078520960ad8c80
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-10-09 01:45:42 +0200

    perf(neovim): [matchup] load on VeryLazy

--- nvim/lua/baggiponte/plugins/matchup.lua
+++ nvim/lua/baggiponte/plugins/matchup.lua
@@ -1,7 +1,7 @@
 return {
   'andymass/vim-matchup',
+  event = 'VeryLazy',
   init = function()
     vim.g.matchup_matchparen_offscreen = { method = 'popup' }
   end,
-  event = { 'BufReadPost', 'BufNewFile' },
 }
