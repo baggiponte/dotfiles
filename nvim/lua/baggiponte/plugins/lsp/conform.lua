edeb6f08e5e198f3cdd6c8e2169e0731c6628a04
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-11-18 23:43:41 +0100

    feat(nvim): use conform.nvim as formatter

---
+++ nvim/lua/baggiponte/plugins/lsp/conform.lua
@@ -0,0 +1,28 @@
+local formatters = {
+  python = { 'ruff_format', 'ruff_fix' },
+  lua = { 'stylua' },
+  json = { 'jq' },
+  just = { 'just' },
+  sh = { 'shfmt' },
+  ['*'] = { 'trim_whitespace', 'codespell' },
+}
+
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format({ async = true, lsp_fallback = true })
+      end,
+      mode = '',
+      desc = '[F]ormat buffer with conform.nvim',
+    },
+  },
+  opts = {
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    formatters_by_ft = formatters,
+  },
+}
