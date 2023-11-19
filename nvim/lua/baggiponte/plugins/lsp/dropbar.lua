0c22ecfc90aab1c606a3ee92d71ad463f2ca59d3
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-11-19 14:20:07 +0100

    refactor(nvim): remove unused plugins

--- nvim/lua/baggiponte/plugins/lsp/dropbar.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'bekaboo/dropbar.nvim',
-  enabled = false, -- requires v0.10
-  event = 'LspAttach',
-  -- optional, but required for fuzzy finder support
-  dependencies = {
-    'nvim-telescope/telescope-fzf-native.nvim',
-  },
-}
--- nvim/lua/baggiponte/plugins/undotree.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'mbbill/undotree',
-  cmd = 'UndotreeToggle',
-  config = function()
-    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle [u]ndoTree' })
-  end,
-}
