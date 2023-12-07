eef5c3f34648ac6eef5b232d6a6b43f4aad7a794
baggiponte <57922983+baggiponte@users.noreply.github.com>
2023-12-08 00:39:04 +0100

    refactor: remove intel setup

--- zsh/include/paths.zsh
+++ zsh/include/paths.zsh
@@ -3,13 +3,7 @@
 # +---------------------+
 
 # MUST be before any "hash" call!
-if [ "$(sysctl -n machdep.cpu.brand_string)" = "Apple M1" ]; then
-	eval "$(/opt/homebrew/bin/brew shellenv)"
-	export HOMEBREW_BUNDLE_FILE="$XDG_CONFIG_HOME/Brewfile.M1"
-else
-	eval "$(/usr/local/bin/brew shellenv)"
-	export HOMEBREW_BUNDLE_FILE="$XDG_CONFIG_HOME/Brewfile.Intel"
-fi
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 export PATH="$PATH:/usr/local/bin:$HOME/.local/bin"
 
