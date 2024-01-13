fdadde03ff3cc874d54059dee1b58a8a086f8a58
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-01-13 22:34:57 +0100

    feat(nvim): configure dap-python

--- nvim/lua/baggiponte/plugins/dap.lua
+++ nvim/lua/baggiponte/plugins/dap.lua
@@ -1,56 +1,79 @@
 local import = require('baggiponte.utils').import
 
+local keys = {
+  {
+    '<leader>ds',
+    function()
+      import('dap').continue()
+    end,
+    desc = 'Start [d]ebugger [s]ession',
+    silent = true,
+    noremap = true,
+  },
+  {
+    '<leader>b',
+    function()
+      import('dap').toggle_breakpoint()
+    end,
+    desc = 'Toggle debugger [b]reakpoint',
+    silent = true,
+    noremap = true,
+  },
+  config = function()
+    local dap = import('dap')
+    local dapui = import('dapui')
+
+    dap.listeners.after.event_initialized['dapui_config'] = function()
+      dapui.open({})
+    end
+
+    dap.listeners.before.event_terminated['dapui_config'] = function()
+      dapui.close({})
+    end
+
+    dap.listeners.before.event_exited['dapui_config'] = function()
+      dapui.close({})
+    end
+  end,
+}
+
 return {
   {
-    'mfussenegger/nvim-dap-python',
-    ft = 'python',
+    'mfussenegger/nvim-dap',
+    keys = keys,
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap' },
     config = function()
-      local dappy = import('dap-python')
-      dappy.setup(vim.fn.stdpath('data') .. '/mason/packages/debugpy/venv/bin/python')
-      dappy.test_runner = 'pytest'
+      local dap = require('dap')
+      local dapui = require('dapui')
+
+      dapui.setup()
+
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+      end
     end,
   },
   {
-    'mfussenegger/nvim-dap',
+    'mfussenegger/nvim-dap-python',
+    ft = 'python',
     dependencies = {
-      'rcarriga/nvim-dap-ui',
-      'theHamsta/nvim-dap-virtual-text',
+      'mfussenegger/nvim-dap',
+      'mfussenegger/nvim-dap-ui',
     },
-    keys = {
-      {
-        '<F5>',
-        function()
-          import('dap').continue()
-        end,
-        desc = 'Start debugger session',
-        silent = true,
-        noremap = true,
-      },
-      {
-        '<leader>b',
-        function()
-          import('dap').toggle_breakpoint()
-        end,
-        desc = 'Toggle debugger [b]reakpoint',
-        silent = true,
-        noremap = true,
-      },
-      config = function()
-        local dap = import('dap')
-        local dapui = import('dapui')
-
-        dap.listeners.after.event_initialized['dapui_config'] = function()
-          dapui.open({})
-        end
-
-        dap.listeners.before.event_terminated['dapui_config'] = function()
-          dapui.close({})
-        end
-
-        dap.listeners.before.event_exited['dapui_config'] = function()
-          dapui.close({})
-        end
-      end,
-    },
+    config = function()
+      local path = require('mason-registry').get_package('debugpy'):get_install_path()
+      require('dap-python').setup(path .. '/venv/bin/python')
+    end,
   },
 }
