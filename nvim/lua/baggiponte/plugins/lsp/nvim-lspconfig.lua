f08550430ba9c741a5f19dc57b520cc93271623b
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-01-15 01:17:16 +0100

    refactor(nvim): remove MasonToolInstaller
    can to from inside mason itself

--- nvim/lua/baggiponte/plugins/lsp/nvim-lspconfig.lua
+++ nvim/lua/baggiponte/plugins/lsp/nvim-lspconfig.lua
@@ -1,20 +1,43 @@
-local servers = require('baggiponte.plugins.lsp.utils.tools').servers
-
 return {
   'neovim/nvim-lspconfig',
   event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
   dependencies = {
     { 'folke/neodev.nvim', opts = {}, ft = { 'lua' } },
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
-  },
-  opts = {
-    servers = servers,
+    {
+      'williamboman/mason-lspconfig.nvim',
+      dependencies = {
+        'williamboman/mason.nvim',
+        build = ':MasonUpdate',
+        cmd = { 'Mason', 'MasonUpdate' },
+        opts = {
+          ui = {
+            border = 'rounded',
+            icons = { package_installed = '✓', package_uninstalled = '✗', package_pending = '⟳' },
+          },
+        },
+        config = function(_, opts)
+          local registry = require('mason-registry')
+          local tools = require('baggiponte.plugins.lsp.utils.tools')
+
+          registry.refresh(function()
+            for _, pkg_name in ipairs(tools) do
+              local pkg = registry.get_package(pkg_name)
+              if not pkg:is_installed() then
+                pkg:install()
+              end
+            end
+          end)
+
+          require('mason').setup(opts)
+        end,
+      },
+    },
   },
   config = function(_, opts)
     local lspconfig = require('lspconfig')
+    local servers = require('baggiponte.plugins.lsp.utils.tools').servers
     local has_cmp, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-    local has_mason, mlsp = pcall(require, 'mason-lspconfig')
+    local _, mlsp = pcall(require, 'mason-lspconfig')
 
     require('lspconfig.ui.windows').default_options.border = 'rounded'
 
@@ -32,26 +55,15 @@ return {
 
     local function setup(server)
       local settings =
-        vim.tbl_deep_extend('force', { capabilities = vim.deepcopy(default_capabilities) }, opts.servers[server] or {})
+        vim.tbl_deep_extend('force', { capabilities = vim.deepcopy(default_capabilities) }, servers[server] or {})
 
       lspconfig[server].setup(settings)
     end
 
-    ---@type string[]
-    local ensure_installed = {}
-
-    for server, server_opts in pairs(opts.servers) do
-      if server_opts.mason == false then
-        setup(server)
-      else
-        ensure_installed[#ensure_installed + 1] = server
-      end
-    end
-
-    if has_mason then
-      -- install missing servers and configures those already installed with mason
-      mlsp.setup({ ensure_installed = ensure_installed, handlers = { setup } })
-    end
+    -- install missing servers and configures those already installed with mason
+    -- TODO: don't need mason-lspconfig since servers are installed upon mason startup. just need to map the mason to
+    -- lspconfig name
+    mlsp.setup({ ensure_installed = vim.tbl_keys(servers), handlers = { setup } })
 
     -- set keympas when LSP is attached
     vim.api.nvim_create_autocmd('LspAttach', {
