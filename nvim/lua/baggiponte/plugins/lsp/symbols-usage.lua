e86dbda47e28b117fbcebd7d36cbccf837daa247
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-01-22 22:22:06 +0100

    refactor(nvim): disable symbols-usage for some lsp

--- nvim/lua/baggiponte/plugins/lsp/symbols-usage.lua
+++ nvim/lua/baggiponte/plugins/lsp/symbols-usage.lua
@@ -1,5 +1,9 @@
 return {
   'Wansmer/symbol-usage.nvim',
   event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
-  opts = {},
+  opts = {
+    disable = {
+      lsp = { 'dockerls', 'docker-compose-language-service', 'yamlls', 'jsonls' },
+    },
+  },
 }
