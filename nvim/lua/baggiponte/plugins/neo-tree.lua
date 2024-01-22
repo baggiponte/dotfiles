637d1c6a9adc6810d7cc0a85850eb348f0c38dff
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-01-22 09:50:40 +0100

    feat(nvim): replace nvim-tree with neo-tree

---
+++ nvim/lua/baggiponte/plugins/neo-tree.lua
@@ -0,0 +1,19 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  cmd = 'Neotree',
+  keys = {
+    {
+      '<leader>N',
+      '<cmd>Neotree toggle right<CR>',
+      mode = { 'n', 'v' },
+      desc = 'Toggle NeoTree',
+    },
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+}
