9bd3eb87c2c8fc2b95442ddcc8f539252d88f76d
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-05-10 18:32:12 +0200

    refactor(zsh): update fzf eval

--- zsh/include/eval.zsh
+++ zsh/include/eval.zsh
@@ -4,6 +4,13 @@ command -v cz >/dev/null && eval "$(register-python-argcomplete cz)"     # conve
 command -v pipx >/dev/null && eval "$(register-python-argcomplete pipx)" # autocompletion for pipx
 command -v python >/dev/null && eval "$(python -m pip completion --zsh)" # autocompletion for pip
 command -v pixi >/dev/null && eval "$(pixi completion --shell zsh)"
+command -v sk >/dev/null && compdef _gnu_generic sk
+
+if command -v fzf >/dev/null; then
+    eval "$(fzf --zsh)"
+    compdef _gnu_generic fzf
+fi
+
 
 # +-------+
 # | HOOKS |
