1af27a62bd77ca7bb362495763f05344d9f1b88a
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-05-18 17:33:45 +0200

    refactor(nvim): remove plugins and keymaps with 0.10

--- nvim/lua/baggiponte/plugins/lsp/utils/handlers.lua
+++ nvim/lua/baggiponte/plugins/lsp/utils/handlers.lua
@@ -2,6 +2,9 @@ local has_cmp, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
 
 local M = {}
 
+-- 0.10.0 defaults:
+-- K -> hover
+-- ]d [d -> next/prev diagnostic
 M.keys = {
   {
     '<leader>rn',
@@ -18,9 +21,8 @@ M.keys = {
     end,
     '[f]ormat current buffer',
   },
-  { '<leader>ca', vim.lsp.buf.code_action, 'Execute [c]ode [a]ction' },
-  { '[d', vim.diagnostic.goto_prev, 'Go to next [d]iagnostic' },
-  { ']d', vim.diagnostic.goto_next, 'Go to previous [d]iagnostic' },
+  { '<leader>e', vim.diagnostic.open_float, 'Open diagnistics floating pane' },
+  { 'ca', vim.lsp.buf.code_action, 'Execute [c]ode [a]ction' },
   { 'gf', '<cmd>Lspsaga finder<CR>', '[g]o [f]ind occurrences with Lspsaga' },
   { 'gp', '<cmd>Lspsaga peek_definition<CR>', '[g]o [p]eek the definition with Lspsaga' },
   { 'gD', vim.lsp.buf.declaration, '[g]o to [D]eclaration' },
@@ -28,8 +30,7 @@ M.keys = {
   { 'gi', vim.lsp.buf.implementation, '[g]o to [i]mplementation' },
   { 'gr', vim.lsp.buf.references, '[g]o to [r]eferences' },
   { 'gt', vim.lsp.buf.type_definition, '[g]o to [t]ype definition' },
-  { 'K', vim.lsp.buf.hover, 'Display documentation on hover' },
-  { '<C-k>', vim.lsp.buf.signature_help, 'go to signature help' },
+  { '<leader>h', vim.lsp.buf.signature_help, 'go to signature help' },
 }
 
 M.on_attach = function(client, buffer)
