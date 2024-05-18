9a4e41a202dc2afa1fc6ee4a4780a24e4e1c48fb
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-05-18 17:33:45 +0200

    refactor(nvim): remove plugins and keymaps with 0.10

--- nvim/lua/baggiponte/plugins/mini.lua
+++ nvim/lua/baggiponte/plugins/mini.lua
@@ -1,27 +1,12 @@
-local import = require('baggiponte.utils').import
-
 return {
-  {
-    'echasnovski/mini.comment',
-    version = false,
-    keys = {
-      { 'gcc', mode = { 'n', 'v' } },
-      { 'gc', mode = { 'n', 'v' } },
-    },
-    config = function()
-      import('mini.comment').setup({})
-    end,
-  },
   {
     'echasnovski/mini.splitjoin',
     version = false,
     keys = { 'gss' },
-    config = function()
-      import('mini.splitjoin').setup({
-        mappings = {
-          toggle = 'gss',
-        },
-      })
-    end,
+    opts = {
+      mappings = {
+        toggle = 'gss',
+      },
+    },
   },
 }
