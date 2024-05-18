c59207f880ee5ec8d33798fa9272db7a1baf5529
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-05-19 00:51:36 +0200

    refactor(nvim): remove mason-lspconfig
    refactor: decouple mason from lspconfig

--- nvim/lua/baggiponte/plugins/lsp/utils/tools.lua
+++ nvim/lua/baggiponte/plugins/lsp/utils/tools.lua
@@ -1,3 +1,16 @@
+---@alias MasonTool string name of a Mason tool (lsp, formatter, linter, debugger)
+---@alias NvimLSPServer string name of a nvim-lspconfig server
+
+---@class MasonServer
+---@field name NvimLSPServer
+---@field config? table
+
+---@class MasonTools
+---@field linters MasonTool[]
+---@field formatters MasonTool[]
+---@field dap MasonTool[]
+---@field servers table<MasonTool, MasonServer>
+---@field all MasonTool[]
 local M = {}
 
 M.linters = {
@@ -20,50 +33,83 @@ M.dap = {
 }
 
 M.servers = {
-  cssls = {},
-  pyright = {
-    settings = {
-      pyright = {
-        -- Using Ruff's import organizer
-        disableOrganizeImports = true,
-      },
-    },
+  ['lua-language-server'] = {
+    name = 'lua_ls',
   },
-  ruff = {},
-  dockerls = {},
-  docker_compose_language_service = {},
-  terraformls = {},
-  tflint = {},
-  lua_ls = {},
-  yamlls = {
-    settings = {
-      yaml = {
-        schemaStore = { enable = true },
-        format = {
-          enable = true,
-          singleQuote = false,
-          bracketSpacing = false,
+  ['css-lsp'] = {
+    name = 'cssls',
+  },
+  ['dockerfile-language-server'] = {
+    name = 'dockerls',
+  },
+  ['docker-compose-language-service'] = {
+    name = 'docker_compose_language_service',
+  },
+  ['terraform-ls'] = {
+    name = 'terraformls',
+  },
+  tflint = {
+    name = 'tflint',
+  },
+  ruff = {
+    name = 'ruff',
+  },
+  pyright = {
+    name = 'pyright',
+    config = {
+      settings = {
+        pyright = {
+          -- Using Ruff's import organizer
+          disableOrganizeImports = true,
         },
       },
     },
   },
-  jsonls = {
-    settings = {
-      json = {
-        validate = { enable = true },
+  ['yaml-language-server'] = {
+    name = 'yamlls',
+    config = {
+      settings = {
+        yaml = {
+          schemaStore = {
+            -- You must disable built-in schemaStore support if you want to use
+            -- schemastore.nvim and its advanced options like `ignore`.
+            enable = false,
+            -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+            url = '',
+          },
+          schemas = function()
+            return require('schemastore').yaml.schemas()
+          end,
+        },
       },
     },
   },
-  rust_analyzer = {
-    cmd = {
-      'rustup',
-      'run',
-      'stable',
-      'rust-analyzer',
+  ['json-lsp'] = {
+    name = 'jsonls',
+    config = {
+      settings = {
+        json = {
+          schemas = function()
+            return require('schemastore').json.schemas()
+          end,
+          validate = { enable = true },
+        },
+      },
+    },
+  },
+  ['rust-analyzer'] = {
+    name = 'rust_analyzer',
+    config = {
+      cmd = {
+        'rustup',
+        'run',
+        'stable',
+        'rust-analyzer',
+      },
     },
   },
 }
 
-M.tools = vim.tbl_flatten(vim.tbl_deep_extend('force', {}, { M.dap, M.linters, M.formatters, vim.tbl_keys(M.servers) }))
+M.all = vim.iter({ M.dap, M.linters, M.formatters, vim.tbl_keys(M.servers) }):flatten():totable()
 
 return M
