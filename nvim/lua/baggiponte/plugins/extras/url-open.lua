34cba53eed67ab59ab2f95396cfd138e3cfe24aa
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-05-20 11:33:10 +0200

    refactor(nvim): remove urlopen
    Can use gx now

--- nvim/lua/baggiponte/plugins/extras/url-open.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'sontungexpt/url-open',
-  cmd = 'URLOpenUnderCursor',
-  -- keys = { '<leader>gx', '<cmd>URLOpenUnderCursor<cr>', desc = 'Open the url under the cursor' }, -- doesn't work
-  opts = {},
-}
