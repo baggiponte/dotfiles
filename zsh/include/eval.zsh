47fb3710efa5ff6bc177709f135552614dad4d21
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-07-10 15:07:55 +0200

    feat(zsh): add azure-cli completion (hopefully)

--- zsh/include/eval.zsh
+++ zsh/include/eval.zsh
@@ -1,9 +1,13 @@
+#
 # `eval` commands are run **after** compinit
+#
+
 # completions
+command -v az /dev/null && eval "$(register-python-argcomplete az)"      # autocompletion for azure-cli
 command -v cz >/dev/null && eval "$(register-python-argcomplete cz)"     # conventional commits + semver + keep a changelog
 command -v pipx >/dev/null && eval "$(register-python-argcomplete pipx)" # autocompletion for pipx
-command -v python >/dev/null && eval "$(python -m pip completion --zsh)" # autocompletion for pip
 command -v pixi >/dev/null && eval "$(pixi completion --shell zsh)"
+command -v python >/dev/null && eval "$(python -m pip completion --zsh)" # autocompletion for pip
 command -v rye >/dev/null && eval "$(rye self completion)"
 command -v sk >/dev/null && compdef _gnu_generic sk
 
