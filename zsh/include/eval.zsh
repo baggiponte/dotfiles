c8427b8a2e9baa040909a625b7c0fc1b6862028c
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-07-10 14:47:04 +0200

    chore(nvim): update lockfile

--- zsh/include/eval.zsh
+++ zsh/include/eval.zsh
@@ -4,6 +4,7 @@ command -v cz >/dev/null && eval "$(register-python-argcomplete cz)"     # conve
 command -v pipx >/dev/null && eval "$(register-python-argcomplete pipx)" # autocompletion for pipx
 command -v python >/dev/null && eval "$(python -m pip completion --zsh)" # autocompletion for pip
 command -v pixi >/dev/null && eval "$(pixi completion --shell zsh)"
+command -v rye >/dev/null && eval "$(rye self completion)"
 command -v sk >/dev/null && compdef _gnu_generic sk
 
 if command -v fzf >/dev/null; then
@@ -18,9 +19,8 @@ fi
 
 # hook binaries into zsh
 command -v direnv >/dev/null && eval "$(direnv hook zsh)"         # autoload dotenv files
-command -v mise >/dev/null && eval "$(mise activate zsh)"         # hook mise (formerly rtx)
+command -v mise >/dev/null && eval "$(mise activate zsh)"         # hook mise
 command -v zoxide >/dev/null && eval "$(zoxide init zsh --cmd=j)" # autojump with j
-command -v rye >/dev/null && eval "$(rye self completion)"
 
 if command -v starship >/dev/null; then
 	export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
