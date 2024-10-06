acaf1a200a679452d8ff8096f1a578ac6fb1f002
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-10-06 19:15:35 +0200

    feat(nvim): try avante
    refactor(nvim): avante config

---
+++ nvim/lua/baggiponte/plugins/avante.lua
@@ -0,0 +1,44 @@
+local dependencies = {
+  'nvim-treesitter/nvim-treesitter',
+  'stevearc/dressing.nvim',
+  'nvim-lua/plenary.nvim',
+  'MunifTanjim/nui.nvim',
+  --- The below dependencies are optional,
+  'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+  'zbirenbaum/copilot.lua', -- for providers='copilot'
+  {
+    -- support for image pasting
+    'HakonHarnes/img-clip.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- recommended settings
+      default = {
+        embed_image_as_base64 = false,
+        prompt_for_file_name = false,
+        drag_and_drop = {
+          insert_mode = true,
+        },
+        -- required for Windows users
+        use_absolute_path = true,
+      },
+    },
+  },
+  {
+    -- Make sure to set this up properly if you have lazy=true
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      file_types = { 'markdown', 'Avante' },
+    },
+    ft = { 'markdown', 'Avante' },
+  },
+}
+
+return {
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
+  dependencies = dependencies,
+  cmd = { 'Avante' },
+  opts = {
+    provider = 'copilot',
+  },
+}
