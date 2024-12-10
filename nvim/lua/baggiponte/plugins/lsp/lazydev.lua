751fb27b06697e5b4cb3a79da224cd260401abdb
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-12-10 15:00:42 +0100

    refactor(nvim): update lazydev config

--- nvim/lua/baggiponte/plugins/lsp/lazydev.lua
+++ nvim/lua/baggiponte/plugins/lsp/lazydev.lua
@@ -1,17 +1,11 @@
 return {
-  {
-    'folke/lazydev.nvim',
-    ft = 'lua', -- only load on lua files
-    opts = {
-      library = {
-        -- Library items can be absolute paths
-        -- "~/projects/my-awesome-lib",
-        -- Or relative, which means they will be resolved as a plugin
-        -- "LazyVim",
-        -- When relative, you can also provide a path to the library in the plugin dir
-        'luvit-meta/library', -- see below
-      },
+  'folke/lazydev.nvim',
+  ft = 'lua', -- only load on lua files
+  opts = {
+    library = {
+      -- See the configuration section for more details
+      -- Load luvit types when the `vim.uv` word is found
+      { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
     },
   },
-  { 'bilal2453/luvit-meta', lazy = true },
 }
