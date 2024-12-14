d185e2c281b34326fbde42610c246be305ce3d2c
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-12-14 13:24:02 +0100

    refactor(nvim): update noice settings

--- nvim/lua/baggiponte/plugins/ui/noice.lua
+++ nvim/lua/baggiponte/plugins/ui/noice.lua
@@ -1,34 +1,23 @@
 return {
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      {
-        'rcarriga/nvim-notify',
-        opts = {
-          render = 'compact',
-          stages = 'slide',
-          top_down = false,
-          -- max_width = math.floor(vim.o.columns * 0.6),
-        },
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  opts = {
+    cmdline = { enabled = true },
+    popupmenu = { enabled = true },
+    notify = { enabled = false },
+    lsp = {
+      enabled = false,
+      progress = { enabled = false },
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
       },
     },
-    opts = {
-      cmdline = { enabled = true },
-      messages = { enabled = true },
-      lsp = {
-        progress = { enabled = false },
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          -- ['cmp.entry.get_documentation'] = true,
-        },
-      },
-      presets = {
-        inc_rename = true,
-        lsp_doc_border = true,
-      },
+    presets = {
+      inc_rename = true,
+      lsp_doc_border = true,
     },
   },
 }
