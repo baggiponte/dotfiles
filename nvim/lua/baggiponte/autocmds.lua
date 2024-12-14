b6da35592946c8265da5d2eb1d9c8b3cf8b4c952
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-12-14 15:35:26 +0100

    docs: add reference to source

--- nvim/lua/baggiponte/autocmds.lua
+++ nvim/lua/baggiponte/autocmds.lua
@@ -22,16 +22,21 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   command = [[%s/\s\+$//e]],
 })
 
----@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
-local progress = vim.defaulttable()
+-- courtesy of folke: https://github.com/folke/snacks.nvim/blob/main/docs/notifier.md#-examples
 vim.api.nvim_create_autocmd('LspProgress', {
+
   ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
     local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+
     if not client or type(value) ~= 'table' then
       return
     end
+
+    ---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+    local progress = vim.defaulttable()
     local p = progress[client.id]
 
     for i = 1, #p + 1 do
@@ -55,6 +60,7 @@ vim.api.nvim_create_autocmd('LspProgress', {
     end, p)
 
     local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+
     vim.notify(table.concat(msg, '\n'), 'info', {
       id = 'lsp_progress',
       title = client.name,
