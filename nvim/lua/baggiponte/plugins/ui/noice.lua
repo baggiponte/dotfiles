a61d013a3ba550529c4b87a2b2b9750cd71af04f
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-12-14 16:29:34 +0100

    chore(nvim): disable more noice stuff

--- nvim/lua/baggiponte/plugins/ui/noice.lua
+++ nvim/lua/baggiponte/plugins/ui/noice.lua
@@ -8,16 +8,10 @@ return {
     notify = { enabled = false },
     lsp = {
       enabled = false,
-      progress = { enabled = false },
-      override = {
-        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-        ['vim.lsp.util.stylize_markdown'] = true,
-        ['cmp.entry.get_documentation'] = true,
-      },
+      hover = { enabled = false },
     },
     presets = {
       inc_rename = true,
-      lsp_doc_border = true,
     },
   },
 }
