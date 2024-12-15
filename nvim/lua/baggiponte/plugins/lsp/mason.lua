3e45f374f54b00fc211329bbdae7a98662a72be2
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-12-15 13:41:51 +0100

    refactor(nvim): do the for loop on LspAttach

--- nvim/lua/baggiponte/plugins/lsp/mason.lua
+++ nvim/lua/baggiponte/plugins/lsp/mason.lua
@@ -19,16 +19,25 @@ return {
   config = function(_, opts)
     require('mason').setup(opts)
 
-    local tools = require('baggiponte.plugins.lsp.utils.tools')
-    local registry = require('mason-registry')
+    -- perhaps this is a chicken and egg problem
+    -- the lsp might not attach if you don't have the server installed
+    --
+    -- the for loop + check adds ~10ms to startup time, so it might
+    -- make sense to disable it until an lsp is actually attached
+    vim.api.nvim_create_autocmd('LspAttach', {
+      callback = function()
+        local tools = require('baggiponte.plugins.lsp.utils.tools')
+        local registry = require('mason-registry')
 
-    registry.refresh(function()
-      vim.iter(tools.all):map(function(tool)
-        local pkg = registry.get_package(tool)
-        if not pkg:is_installed() then
-          pkg:install()
-        end
-      end)
-    end)
+        registry.refresh(function()
+          vim.iter(tools.all):map(function(tool)
+            local pkg = registry.get_package(tool)
+            if not pkg:is_installed() then
+              pkg:install()
+            end
+          end)
+        end)
+      end
+    })
   end,
 }
