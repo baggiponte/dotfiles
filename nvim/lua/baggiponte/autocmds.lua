23c49caaa114e3ea5918cf09f9ef3f1beac40a62
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-12-15 13:20:43 +0100

    fix(nvim): remove redundant autocmd

--- nvim/lua/baggiponte/autocmds.lua
+++ nvim/lua/baggiponte/autocmds.lua
@@ -7,15 +7,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- close some filetypes with <q>
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'TelescopePrompt' },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set('n', 'q', '<cmd>close!<cr>', { buffer = event.buf, silent = true })
-  end,
-})
-
 -- Automatic removal of trailing whitespace
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   pattern = { '*' },
