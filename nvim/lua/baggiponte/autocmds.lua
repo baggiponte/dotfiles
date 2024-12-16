28612f85a15db295b4dc609da3c3748aaa3c59ab
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-12-16 08:48:08 +0100

    style: run stylua

--- nvim/lua/baggiponte/autocmds.lua
+++ nvim/lua/baggiponte/autocmds.lua
@@ -20,8 +20,7 @@ vim.api.nvim_create_autocmd('LspProgress', {
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
-    local value = ev.data.params
-    .value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+    local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
 
     if not client or type(value) ~= 'table' then
       return
@@ -58,7 +57,7 @@ vim.api.nvim_create_autocmd('LspProgress', {
       title = client.name,
       opts = function(notif)
         notif.icon = #progress[client.id] == 0 and ' '
-            or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+          or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
       end,
     })
   end,
