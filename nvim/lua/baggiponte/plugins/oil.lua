8d11f94801bfcb2d17ccc1e20ba721dbaf8f29b1
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-12-19 11:50:26 +0100

    refactor(nvim): add keymap for oil

--- nvim/lua/baggiponte/plugins/oil.lua
+++ nvim/lua/baggiponte/plugins/oil.lua
@@ -1,6 +1,9 @@
 return {
   'stevearc/oil.nvim',
   cmd = 'Oil',
+  keys = {
+    { '<leader>o', '<CMD>Oil<CR>' },
+  },
   opts = {
     show_hidden = true,
     is_always_hidden = function(name, bufnr)
