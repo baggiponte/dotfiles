a0f13235c2dd83f6252621108765ea1eb30562e4
baggiponte <57922983+baggiponte@users.noreply.github.com>
2024-12-19 11:50:42 +0100

    refactor(nvim): change keybinding for symbols outline

--- nvim/lua/baggiponte/plugins/lsp/outline.lua
+++ nvim/lua/baggiponte/plugins/lsp/outline.lua
@@ -3,7 +3,7 @@ return {
   lazy = true,
   cmd = { 'Outline', 'OutlineOpen' },
   keys = { -- Example mapping to toggle outline
-    { '<leader>o', '<cmd>Outline<CR>', desc = 'Toggle outline' },
+    { 'gO', '<cmd>Outline<CR>', desc = 'Toggle outline' },
   },
   opts = {
     -- Your setup opts here
