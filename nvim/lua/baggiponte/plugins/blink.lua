f1b390fde3e563b794eabb0c4f91234831989ce1
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-01-05 18:36:11 +0100

    feat(nvim): use blink
    fix(nvim): add dependencies
    
    fix(nvim): use proper plugin name

---
+++ nvim/lua/baggiponte/plugins/blink.lua
@@ -0,0 +1,58 @@
+return {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    { 'giuxtaposition/blink-cmp-copilot', dependencies = { 'zbirenbaum/copilot.lua' } },
+  },
+
+  -- use a release tag to download pre-built binaries
+  version = '*',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- See the full "keymap" documentation for information on defining your own keymap.
+    keymap = {
+      preset = 'default',
+
+      ['['] = { 'select_prev', 'fallback' },
+      [']'] = { 'select_next', 'fallback' },
+      ['<C-n>'] = { 'select_and_accept' },
+      ['<C-p>'] = {},
+      ['<C-y>'] = {},
+    },
+
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- Will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono',
+    },
+
+    completion = {
+      menu = { border = 'rounded' },
+      documentation = { window = { border = 'rounded' } },
+    },
+    signature = { window = { border = 'rounded' } },
+
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+      providers = {
+        copilot = {
+          name = 'copilot',
+          module = 'blink-cmp-copilot',
+          score_offset = 100,
+          async = true,
+        },
+      },
+    },
+  },
+  opts_extend = { 'sources.default' },
+}
