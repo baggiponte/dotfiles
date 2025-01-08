e831ff7ffa287ea4ce5ae7b871ea53ac8ed80b06
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-01-08 18:53:52 +0100

    fix(zsh): autoload only after generating the compdef files

--- zsh/include/eval.zsh
+++ zsh/include/eval.zsh
@@ -1,6 +1,3 @@
-autoload -Uz compinit && compinit -d "${XDG_CACHE_HOME}/zsh/zcompdump"
-autoload -Uz bashcompinit && bashcompinit
-
 # +-------------+
 # | COMPLETIONS |
 # +-------------+
@@ -12,15 +9,11 @@ COMPDIR="${XDG_CACHE_HOME}/zsh/zfunc"
 # $XDG_CACHE_HOME/zsh/zcompdump and run compinit
 fpath=(${COMPDIR} $fpath) # won't work if there are quotes
 
-
 # python
-command -v python >/dev/null && eval "$(python -m pip completion --zsh)" # autocompletion for pip
-
 command -v uv >/dev/null && uv generate-shell-completion zsh > "${COMPDIR}/_uv"
 command -v uvx >/dev/null && uvx --generate-shell-completion=zsh > "${COMPDIR}/_uvx"
 command -v ruff >/dev/null && ruff generate-shell-completion zsh > "${COMPDIR}/_ruff"
 
-command -v sky >/dev/null && source ~/.sky/.sky-complete.zsh
 command -v cz >/dev/null && register-python-argcomplete cz > "${COMPDIR}/_cz"
 
 # rust
@@ -28,7 +21,6 @@ command -v rustup >/dev/null && rustup completions zsh > "${COMPDIR}/_rustup"
 command -v cargo >/dev/null && rustup completions zsh cargo > "${COMPDIR}/_cargo"
 
 # misc
-
 command -v runpodctl >/dev/null && runpodctl completion zsh > "${COMPDIR}/_runpodctl"
 command -v az >/dev/null && register-python-argcomplete az > "${COMPDIR}/_az"
 command -v zellij >/dev/null && zellij setup --generate-completion zsh > "${COMPDIR}/_zellij"
@@ -36,16 +28,22 @@ command -v devpod >/dev/null && devpod completion zsh > "${COMPDIR}/_devpod"
 command -v arduino-cli >/dev/null && arduino-cli completion zsh > "${COMPDIR}/_arduino-cli"
 command -v sk >/dev/null && compdef _gnu_generic sk
 
+autoload -Uz compinit && compinit -d "${XDG_CACHE_HOME}/zsh/zcompdump"
+autoload -Uz bashcompinit && bashcompinit
+
+# these require compdef
+command -v python >/dev/null && eval "$(python -m pip completion --zsh)"
+
+if command -v sky >/dev/null && [[ -s ~/.sky/.sky-complete.zsh ]]; then
+    source ~/.sky/.sky-complete.zsh
+elif ! [[ -s ~/.sky/.sky-complete.zsh ]]; then
+    echo "skypilot compdef not found. Run 'sky --install-shell-completion' to install"
+fi
+
 if command -v fzf >/dev/null; then
     eval "$(fzf --zsh)"
     compdef _gnu_generic fzf
 fi
-
-# unused
-command -v rye >/dev/null && rye self completion -s zsh > "${COMPDIR}/_rye"
-command -v pdm >/dev/null &&  pdm completion zsh > "${COMPDIR}/_pdm"
-command -v pixi >/dev/null &&  pixi completion --shell zsh > "${COMPDIR}/_pixi"
-command -v pipx >/dev/null && register-python-argcomplete pipx > "${COMPDIR}/_pipx"
 
 # +-------+
 # | HOOKS |
@@ -58,7 +56,3 @@ if command -v starship >/dev/null; then
 	export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 	eval "$(starship init zsh)"
 fi
-
-# unused
-command -v direnv >/dev/null && eval "$(direnv hook zsh)"         # autoload dotenv files
-command -v mise >/dev/null && eval "$(mise activate zsh)"         # hook mise
