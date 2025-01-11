5ca9319231255a11aa58c7a8e86a251a851e673b
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-01-11 19:41:56 +0100

    refactor(nvim): just use one file for telescope

--- nvim/lua/baggiponte/plugins/telescope.lua
+++ nvim/lua/baggiponte/plugins/telescope.lua
@@ -1,4 +1,59 @@
-local keys = require('baggiponte.plugins.telescope.keys')
+local keys = {
+  {
+    '<leader>ff',
+    function()
+      require('telescope.builtin').find_files()
+    end,
+    desc = 'Telescope find files',
+    silent = true,
+    noremap = true,
+  },
+  {
+    '<leader>fg',
+    function()
+      require('telescope.builtin').live_grep()
+    end,
+    desc = 'Telescope find symbol using grep',
+    silent = true,
+    noremap = true,
+  },
+  {
+    '<leader>fb',
+    function()
+      require('telescope.builtin').buffers()
+    end,
+    desc = 'Telescope find buffer',
+    silent = true,
+    noremap = true,
+  },
+  {
+    '<leader>fd',
+    function()
+      require('telescope.builtin').diagnostics()
+    end,
+    desc = 'Telescope find in workspace diagnostics',
+    silent = true,
+    noremap = true,
+  },
+  {
+    '<leader>fh',
+    function()
+      require('telescope.builtin').help_tags()
+    end,
+    desc = 'Telescope help tags',
+    silent = true,
+    noremap = true,
+  },
+  {
+    '<leader>fw',
+    function()
+      require('telescope.builtin').grep_string()
+    end,
+    desc = 'Telescope grep string',
+    silent = true,
+    noremap = true,
+  },
+}
 
 return {
   'nvim-telescope/telescope.nvim',
