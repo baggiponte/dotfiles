5d2344c1b7cf65958bf706416104dea1cca5cab1
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-01-11 19:36:58 +0100

    refactor(nvim): update outline keymaps

--- nvim/lua/baggiponte/plugins/lsp/outline.lua
+++ nvim/lua/baggiponte/plugins/lsp/outline.lua
@@ -1,9 +1,8 @@
 return {
   'hedyhli/outline.nvim',
-  lazy = true,
   cmd = { 'Outline', 'OutlineOpen' },
   keys = { -- Example mapping to toggle outline
-    { 'gO', '<cmd>Outline<CR>', desc = 'Toggle outline' },
+    { 'go', '<cmd>Outline<CR>', desc = 'Toggle outline' },
   },
   opts = {
     -- Your setup opts here
