c4ca81768102a5cfc11dc43b15638543bbe23980
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-01-14 21:35:41 +0100

    feat(nvim): add rename LSP capabilities

--- nvim/lua/baggiponte/plugins/neo-tree.lua
+++ nvim/lua/baggiponte/plugins/neo-tree.lua
@@ -10,4 +10,15 @@ return {
       desc = 'Toggle NeoTree',
     },
   },
+  opts = function(_, opts)
+    local function on_move(data)
+      Snacks.rename.on_rename_file(data.source, data.destination)
+    end
+    local events = require('neo-tree.events')
+    opts.event_handlers = opts.event_handlers or {}
+    vim.list_extend(opts.event_handlers, {
+      { event = events.FILE_MOVED,   handler = on_move },
+      { event = events.FILE_RENAMED, handler = on_move },
+    })
+  end,
 }
