6d45ccc9ac1b0323ce35efb9d56d6cce1f29c59c
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-01-14 21:04:29 +0100

    fix(nvim): load mason on lsp attach

--- nvim/lua/baggiponte/plugins/lsp/nvim-lspconfig.lua
+++ nvim/lua/baggiponte/plugins/lsp/nvim-lspconfig.lua
@@ -65,13 +65,16 @@ return {
   'neovim/nvim-lspconfig',
   event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
   dependencies = {
-    'folke/lazydev.nvim',
-    ft = 'lua', -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+    'williamboman/mason.nvim',
+    {
+      'folke/lazydev.nvim',
+      ft = 'lua', -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        },
       },
     },
   },
