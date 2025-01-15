30ccbc4f101c6bbb001eb92f8445521965a23fad
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-01-15 16:29:03 +0100

    chore(blink): launch on event

--- nvim/lua/baggiponte/plugins/blink.lua
+++ nvim/lua/baggiponte/plugins/blink.lua
@@ -1,5 +1,6 @@
 return {
   'saghen/blink.cmp',
+  event = { 'InsertEnter', 'CmdlineEnter' },
   -- optional: provides snippets for the snippet source
   dependencies = {
     'rafamadriz/friendly-snippets',
