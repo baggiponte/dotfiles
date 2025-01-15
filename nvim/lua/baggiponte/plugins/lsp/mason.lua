08146262a8671bcfd6fae63ea966f11e04e6c56a
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-01-15 16:31:19 +0100

    chore(nvim): use workaround for mason

--- nvim/lua/baggiponte/plugins/lsp/mason.lua
+++ nvim/lua/baggiponte/plugins/lsp/mason.lua
@@ -52,26 +52,6 @@ return {
   },
   config = function(_, opts)
     require('mason').setup(opts.plugin)
-
-    -- perhaps this is a chicken and egg problem
-    -- the lsp might not attach if you don't have the server installed
-    --
-    -- the for loop + check adds ~10ms to startup time, so it might
-    -- make sense to disable it until an lsp is actually attached
-    vim.api.nvim_create_autocmd('LspAttach', {
-      callback = function()
-        local tools = vim.iter({ opts.debuggers, opts.linters, opts.formatters, opts.servers }):flatten():totable()
-        local registry = require('mason-registry')
-
-        registry.refresh(function()
-          vim.iter(tools.all):map(function(tool)
-            local pkg = registry.get_package(tool)
-            if not pkg:is_installed() then
-              pkg:install()
-            end
-          end)
-        end)
-      end,
-    })
+    -- TODO: autocmd on LSPAttach (or VeryLazy) to install required tools
   end,
 }
