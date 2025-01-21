0fe695a8f5ebebab5ed660314d31ad06a8e7da2e
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-01-21 15:22:54 +0100

    feat(nvim): try dadbod
    not really without using bigquery

---
+++ nvim/lua/baggiponte/plugins/dadbod.lua
@@ -0,0 +1,9 @@
+return {
+  'tpope/vim-dadbod',
+  cmd = { 'DB', 'DBUI' },
+  dependencies = {
+    'kristijanhusak/vim-dadbod-completion',
+    'kristijanhusak/vim-dadbod-ui',
+  },
+  opts = {},
+}
