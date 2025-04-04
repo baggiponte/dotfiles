af440ae4b21c105b021a879e2a402a49f982be4e
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-04-04 10:58:47 +0200

    style: format with stylua

--- nvim/lua/baggiponte/plugins/neo-tree.lua
+++ nvim/lua/baggiponte/plugins/neo-tree.lua
@@ -17,7 +17,7 @@ return {
     local events = require('neo-tree.events')
     opts.event_handlers = opts.event_handlers or {}
     vim.list_extend(opts.event_handlers, {
-      { event = events.FILE_MOVED,   handler = on_move },
+      { event = events.FILE_MOVED, handler = on_move },
       { event = events.FILE_RENAMED, handler = on_move },
     })
   end,
