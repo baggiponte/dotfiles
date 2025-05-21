6734e9772e8a9a200047a20c7315e6a4a9e13f86
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-05-21 17:34:18 +0200

    chore(nvim): remove unused colorschemes

--- nvim/lua/baggiponte/plugins/ui/colorscheme.lua
+++ nvim/lua/baggiponte/plugins/ui/colorscheme.lua
@@ -1,49 +1,25 @@
 return {
-  {
-    'sainnhe/gruvbox-material',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- defaults
-      -- vim.g.gruvbox_material_background = 'medium'
-      -- vim.g.gruvbox_material_diagnostic_line_highlight = 1
-      -- vim.g.gruvbox_material_diagnostic_text_highlight = 0
-      -- vim.g.gruvbox_material_disable_italic_comment = 0
-      -- vim.g.gruvbox_material_enable_bold = 0
-      -- vim.g.gruvbox_material_foreground = 'material'
-      -- vim.g.gruvbox_material_ui_contrast = 'low'
+  'sainnhe/gruvbox-material',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- defaults
+    -- vim.g.gruvbox_material_background = 'medium'
+    -- vim.g.gruvbox_material_diagnostic_line_highlight = 1
+    -- vim.g.gruvbox_material_diagnostic_text_highlight = 0
+    -- vim.g.gruvbox_material_disable_italic_comment = 0
+    -- vim.g.gruvbox_material_enable_bold = 0
+    -- vim.g.gruvbox_material_foreground = 'material'
+    -- vim.g.gruvbox_material_ui_contrast = 'low'
 
-      vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_diagnostic_virtual_text = 'highlighted'
-      vim.g.gruvbox_material_enable_italic = 1
-      vim.g.gruvbox_material_transparent_background = 2
-      vim.cmd([[colorscheme gruvbox-material]])
+    vim.g.gruvbox_material_better_performance = 1
+    vim.g.gruvbox_material_diagnostic_virtual_text = 'highlighted'
+    vim.g.gruvbox_material_enable_italic = 1
+    vim.g.gruvbox_material_transparent_background = 2
+    vim.cmd([[colorscheme gruvbox-material]])
 
-      for _, element in ipairs({ 'PMenu', 'FloatBorder', 'NormalFloat' }) do
-        vim.api.nvim_set_hl(0, element, { bg = 'none' })
-      end
-    end,
-  },
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    config = function()
-      require('rose-pine').setup({
-        styles = { transparency = true },
-      })
-
-      vim.cmd('colorscheme rose-pine')
-    end,
-  },
-  {
-    'AlexvZyl/nordic.nvim',
-    opts = {
-      transparent_bg = true,
-      bright_border = true,
-      style = 'classic',
-      telescope = {
-        style = 'classic',
-      },
-    },
-  },
+    for _, element in ipairs({ 'PMenu', 'FloatBorder', 'NormalFloat' }) do
+      vim.api.nvim_set_hl(0, element, { bg = 'none' })
+    end
+  end,
 }
