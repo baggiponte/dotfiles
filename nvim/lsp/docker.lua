4b054d64a0c61d0a6145915e53a4085431f6ab4f
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-05-21 23:20:55 +0200

    refactor(nvim): just use bunx/uvx to launch servers

--- nvim/lsp/docker.lua
+++ nvim/lsp/docker.lua
@@ -1,9 +1,6 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { 'docker-langserver', '--stdio' },
-  root_markers = {
-    '.git',
-    'Dockerfile',
-  },
+  cmd = { 'bunx', 'docker-langserver', '--stdio' },
+  root_markers = { 'Dockerfile' },
   filetypes = { 'dockerfile' },
 }
