4811f350f4f508db5ac23fc0b482cf58b664699f
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-05-22 00:01:16 +0200

    refactor(nvim): use uvx to launch py servers

--- nvim/lsp/basedpyright.lua
+++ nvim/lsp/basedpyright.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { 'uvx', 'basedpyright-langserver', '--stdio' },
+  cmd = { 'uvx', '--from=basedpyright', '--', 'basedpyright-langserver', '--stdio' },
   root_markers = {
     '.git',
     'pyproject.toml',
