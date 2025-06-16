48c23ab0c25136d14dac01b33229f4efd9d29de1
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-06-16 22:43:04 +0200

    refactor(zsh): move as many functions as possible to scripts in ~/.local/bin

--- zsh/include/functions.zsh
+++ zsh/include/functions.zsh
@@ -2,29 +2,6 @@
 # | Almost always require an external command |
 # +-------------------------------------------+
 
-CMD_PREVIEW_EZA="eza --all --group-directories-first --icons --level=2 --tree --ignore-glob=\"$IGNORES_EZA\" -- {}"
-
-# find files and pipe in sk/fzf preview with bat
-zoxide-interactive() {
-	requires zoxide fzf
-
-	local query="$1"
-	local chosen_directory
-
-	chosen_directory=$(
-		zoxide query \
-			--exclude "$PWD" \
-			--list |
-        fzf \
-            --preview="$CMD_PREVIEW_EZA" \
-            --height 40% \
-            --preview-window=down,40% \
-            --query="$query"
-	)
-
-	cd -- "$chosen_directory" || return 1
-}
-
 # +-------+
 # | Utils |
 # +-------+
