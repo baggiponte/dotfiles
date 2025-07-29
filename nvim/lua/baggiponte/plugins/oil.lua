36f415534a2388e6e6e176dfe50cfdb04aa6e962
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-07-29 11:26:17 +0200

    chore(nvim): update oil config

--- nvim/lua/baggiponte/plugins/oil.lua
+++ nvim/lua/baggiponte/plugins/oil.lua
@@ -9,10 +9,18 @@ return {
     },
   },
   opts = {
-    show_hidden = true,
-    is_always_hidden = function(name, bufnr)
-      return name == '.git'
-    end,
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = function(name, bufnr)
+        local must_hide = { '.git' }
+
+        for _, v in pairs(must_hide) do
+          if name == v then
+            return true
+          end
+        end
+      end,
+    },
   },
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
