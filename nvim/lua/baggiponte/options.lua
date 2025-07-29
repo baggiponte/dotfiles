59e9c7219af162b2cdacabba84ec452c03a5ca97
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-07-29 15:01:41 +0200

    chore(nvim): remove localleader
    was only used for grug-far which I am not using anymore

--- nvim/lua/baggiponte/options.lua
+++ nvim/lua/baggiponte/options.lua
@@ -27,7 +27,6 @@ end
 -- It's a good idea to set this early in your config, because otherwise if you have
 -- any mappings you set BEFORE doing this, they will be set to the OLD leader.
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ';'
 
 -- Check :h nvim-defaults first!
 -- [[ Sidebar ]]
