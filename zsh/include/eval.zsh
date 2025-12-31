fc87156fc258e6290aba714aadac7386313c38ef
baggiponte <57922983+baggiponte@users.noreply.github.com>
2025-12-31 12:26:05 +0100

    refactor(zsh): moar eval cleanup

--- zsh/include/eval.zsh
+++ zsh/include/eval.zsh
@@ -14,39 +14,25 @@ command -v uv >/dev/null && uv generate-shell-completion zsh > "${COMPDIR}/_uv"
 command -v uvx >/dev/null && uvx --generate-shell-completion=zsh > "${COMPDIR}/_uvx"
 command -v ruff >/dev/null && ruff generate-shell-completion zsh > "${COMPDIR}/_ruff"
 
-command -v cz >/dev/null && register-python-argcomplete cz > "${COMPDIR}/_cz"
-
 # rust
 command -v rustup >/dev/null && rustup completions zsh > "${COMPDIR}/_rustup"
 command -v cargo >/dev/null && rustup completions zsh cargo > "${COMPDIR}/_cargo"
 
 # misc
 command -v az >/dev/null && register-python-argcomplete az > "${COMPDIR}/_az"
-if command -v wt >/dev/null 2>&1; then eval "$(command wt config shell init zsh)"; fi
 
 autoload -Uz compinit && compinit -d "${XDG_CACHE_HOME}/zsh/zcompdump"
 autoload -Uz bashcompinit && bashcompinit
 
 # these require compdef
 command -v python >/dev/null && eval "$(python -m pip completion --zsh)"
-command -v sk >/dev/null && compdef _gnu_generic sk
-
-if command -v sky >/dev/null; then
-    if [[ -s ~/.sky/.sky-complete.zsh ]]; then
-        source ~/.sky/.sky-complete.zsh
-    elif ! [[ -s ~/.sky/.sky-complete.zsh ]]; then
-        echo "skypilot compdef not found. Run 'sky --install-shell-completion' to install"
-    fi
-fi
 
 if command -v fzf >/dev/null; then
     eval "$(fzf --zsh)"
     compdef _gnu_generic fzf
 fi
 
-# +-------+
-# | HOOKS |
-# +-------+
+if command -v wt >/dev/null 2>&1; then eval "$(command wt config shell init zsh)"; fi
 
 # hook binaries into zsh
 command -v zoxide >/dev/null && eval "$(zoxide init zsh --cmd=j)" # autojump with j
