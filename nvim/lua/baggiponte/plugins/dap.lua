6cb839525419ae7006ec6626165b944598f7c12e
baggiponte <57922983+baggiponte@users.noreply.github.com>
2026-01-12 15:47:54 +0100

    refactor(nvim): new dap with UI

--- nvim/lua/baggiponte/plugins/dap.lua
+++ nvim/lua/baggiponte/plugins/dap.lua
@@ -1,89 +1,129 @@
 local keys = {
+  -- Session Control
   {
     '<leader>ds',
     function()
       require('dap').continue()
     end,
-    desc = 'nvim-dap: Start debugger session',
-    silent = true,
-    noremap = true,
+    desc = 'DAP: Start/Continue',
   },
   {
-    '<leader>dt',
+    '<leader>dr',
     function()
-      require('dapui').toggle()
+      require('dap').restart()
     end,
-    desc = 'nvim-dap: debugger toggle',
+    desc = 'DAP: Restart Session',
   },
   {
-    '<leader>b',
+    '<leader>dq',
+    function()
+      require('dap').terminate()
+      require('dap-view').close()
+    end,
+    desc = 'DAP: Terminate Session',
+  },
+
+  -- Stepping
+  {
+    '<leader>dn',
+    function()
+      require('dap').step_over()
+    end,
+    desc = 'DAP: Step Over (Next)',
+  },
+  {
+    '<leader>di',
+    function()
+      require('dap').step_into()
+    end,
+    desc = 'DAP: Step Into',
+  },
+  {
+    '<leader>do',
+    function()
+      require('dap').step_out()
+    end,
+    desc = 'DAP: Step Out',
+  },
+
+  -- Breakpoints
+  {
+    '<leader>db',
     function()
       require('dap').toggle_breakpoint()
     end,
-    desc = 'nvim-dap: Toggle debugger breakpoint',
-    silent = true,
-    noremap = true,
+    desc = 'DAP: Toggle Breakpoint',
+  },
+
+  -- UI & Inspection
+  {
+    '<leader>dv',
+    function()
+      require('dap-view').toggle()
+    end,
+    desc = 'DAP: Toggle View',
+  },
+  {
+    '<leader>dk',
+    function()
+      require('dap.ui.widgets').hover()
+    end,
+    desc = 'DAP: Hover Variables',
+  },
+}
+
+local dap_views_opt = {
+  -- Moves the main debug pane (terminal/repl) to the right side
+  windows = {
+    terminal = {
+      position = 'right', -- Swaps from default 'left'
+      size = 50, -- Adjust this percentage as needed
+    },
+  },
+  -- Reorders the tabs in the winbar
+  winbar = {
+    show = true,
+    sections = {
+      'repl', -- REPL is now first
+      'watches', -- Watches is now second
+      'scopes',
+      'exceptions',
+      'breakpoints',
+      'threads',
+    },
+    default_section = 'repl',
+    controls = {
+      enabled = true,
+    },
   },
 }
 
 return {
-  {
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'igorlfs/nvim-dap-view',
     'mfussenegger/nvim-dap-python',
-    enabled = false,
-    ft = 'python',
-    dependencies = {
-      'mfussenegger/nvim-dap',
-    },
-    config = function()
-      require('dap-python').setup('uv')
-    end,
+    'theHamsta/nvim-dap-virtual-text',
   },
-  {
-    'mfussenegger/nvim-dap',
-    keys = keys,
-    dependencies = {
-      { 'theHamsta/nvim-dap-virtual-text', opts = {} },
-      {
-        'rcarriga/nvim-dap-ui',
-        dependencies = { 'nvim-neotest/nvim-nio' },
-        opts = {
-          icons = {
-            expanded = '',
-            collapsed = '',
-            current_frame = '',
-          },
-          controls = {
-            enabled = true,
-            element = 'console',
-            icons = {
-              pause = '',
-              play = '',
-              step_into = '',
-              step_over = '',
-              step_out = '',
-              step_back = '',
-              run_last = '',
-              terminate = '',
-            },
-          },
-        },
-      },
-    },
-    config = function()
-      local dap, dapui = require('dap'), require('dapui')
+  keys = keys,
+  ft = { 'python' },
+  config = function()
+    local dap = require('dap')
+    local dap_view = require('dap-view')
+    local dap_python = require('dap-python')
 
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-    end,
-  },
+    -- 1. Setup the UI
+    dap_view.setup(dap_views_opt)
+
+    -- 2. Setup Python with uv
+    dap_python.setup('uv')
+    dap_python.test_runner = 'pytest'
+
+    -- 3. (Optional) Auto-open/close the view
+    -- Note: dap-view users often prefer manual toggling,
+    -- but you can automate it like this:
+    dap.listeners.after.event_initialized['dapui_config'] = function()
+      dap_view.open()
+    end
+  end,
 }
