9b88a2fd49748e63205c12b2cafbbece45c95612
baggiponte <57922983+baggiponte@users.noreply.github.com>
2026-01-13 10:22:59 +0100

    refactor(nvim): activate zizmor only inside github workflows

--- nvim/lsp/zizmor.lua
+++ nvim/lsp/zizmor.lua
@@ -2,8 +2,18 @@
 return {
   cmd = { 'uvx', '--from=zizmor', '--', 'zizmor', '--lsp' },
   filetypes = { 'yaml' },
-  root_markers = {
-    '.workflows/',
-    '.git',
-  },
+  -- Gate activation to .workflows/*.yaml; root_dir returning nil prevents attach.
+  single_file_support = false,
+  root_dir = function(fname)
+    if not fname:match('/%.workflows/.*%.ya?ml$') then
+      return nil
+    end
+
+    local git_root = vim.fs.find('.git', { path = fname, upward = true })[1]
+    if git_root then
+      return vim.fs.dirname(git_root)
+    end
+
+    return vim.fs.dirname(fname)
+  end,
 }
