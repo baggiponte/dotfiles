f84b432da4ebdf4306ef6dce4801aabba1241752
baggiponte <57922983+baggiponte@users.noreply.github.com>
2026-01-26 16:47:42 +0100

    Revert "feat: inject sql in python strings"
    This reverts commit f7b64d7aac5d7b1c7136bc781b8cf72767cd046f.

--- nvim/after/queries/python/injections.scm
+++
@@ -1,8 +0,0 @@
-; extends
-
-; Inject SQL into any Python string that looks like SQL.
-; Matches triple-quoted strings too (they are still `string` with `string_content`).
-(string
-  (string_content) @injection.content
-  (#set! injection.language "sql")
-  (#set! injection.combined))
