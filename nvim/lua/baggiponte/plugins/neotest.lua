db498bb9c104a7dbf262484ff536e3e6d7715d7c
baggiponte <57922983+baggiponte@users.noreply.github.com>
2026-04-24 17:07:58 +0200

    fix(nvim): add nvim-nio

--- nvim/lua/baggiponte/plugins/neotest.lua
+++ nvim/lua/baggiponte/plugins/neotest.lua
@@ -25,6 +25,7 @@ return {
   'nvim-neotest/neotest',
   keys = keys,
   dependencies = {
+    'nvim-neotest/nvim-nio',
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
     'nvim-neotest/neotest-python',
